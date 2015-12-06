4ef8be40dcf6b78387e6914cfbb87afa5e9b9f27
zdm <zdm@softvisio.net>
2015-12-07 01:53:19 +0200

    resources updater merged to the single script

--- bin/update-pcore-res.pl
+++ bin/update-pcore-res.pl
@@ -5,9 +5,20 @@ package main v0.1.0;
 use Pcore;
 use Pcore::Util::URI::Host;
 
+# update GeoIP
+P->geoip->update;
+
+# update TLD
 my $cv = AE::cv;
 
 Pcore::Util::URI::Host::tlds($cv);
+
+$cv->recv;
+
+# update pub. suffixes, should be updated after TLDs
+$cv = AE::cv;
+
+Pcore::Util::URI::Host::pub_suffixes($cv);
 
 $cv->recv;
 
