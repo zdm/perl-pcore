cc2d8c2d22f90939d7a82854b881d3377840d8d7
zdm <zdm@softvisio.net>
2015-12-11 19:48:34 +0200

    version formatted

--- lib/Pcore/Core/CLI.pm
+++ lib/Pcore/Core/CLI.pm
@@ -533,7 +533,7 @@ sub help_usage ( $self, $invalid_options = undef ) {
 
 sub help_version ($self) {
     my $format_info = sub ($dist) {
-        return "@{[$dist->name]}-@{[$dist->version]} @{[$dist->revision]} @{[$dist->build_date]}";
+        return "@{[$dist->name]} @{[$dist->version]} @{[$dist->revision]} @{[$dist->build_date]}";
     };
 
     say $format_info->( $PROC->dist ) if $PROC->dist;
