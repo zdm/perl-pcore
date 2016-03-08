98ba6b10ee730a24aa5c8e0a2293aba4a11db505
zdm <zdm@softvisio.net>
2016-03-08 16:41:18 +0200

    crawler removed

---
+++ lib/Pcore/API.pm
@@ -0,0 +1,26 @@
+package Pcore::API;
+
+use Pcore -class;
+use Pcore::API::Response;
+
+1;
+__END__
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Pcore::API - Pcore API base class
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 ATTRIBUTES
+
+=head1 METHODS
+
+=head1 SEE ALSO
+
+=cut
