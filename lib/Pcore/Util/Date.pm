7b6a7ac21548176c3f230869a78164fb1a3b95c5
zdm <zdm@softvisio.net>
2016-03-13 13:07:05 +0200

    from_strptime added

--- lib/Pcore/Util/Date.pm
+++ lib/Pcore/Util/Date.pm
@@ -3,14 +3,52 @@ package Pcore::Util::Date;
 use Pcore;
 use base qw[Time::Moment];
 
+# %a - The abbreviated weekday name ('Sun')
+# %A - The  full  weekday  name ('Sunday')
+# %b - The abbreviated month name ('Jan')
+# %B - The  full  month  name ('January')
+# %c - The preferred local date and time representation
+# %d - Day of the month (01..31)
+# %e - Day of the month without leading zeroes (1..31)
+# %H - Hour of the day, 24-hour clock (00..23)
+# %I - Hour of the day, 12-hour clock (01..12)
+# %j - Day of the year (001..366)
+# %k - Hour of the day, 24-hour clock w/o leading zeroes ( 0..23)
+# %l - Hour of the day, 12-hour clock w/o leading zeroes ( 1..12)
+# %m - Month of the year (01..12)
+# %M - Minute of the hour (00..59)
+# %p - Meridian indicator ('AM'  or  'PM')
+# %P - Meridian indicator ('am'  or  'pm')
+# %S - Second of the minute (00..60)
+# %U - Week  number  of the current year, starting with the first Sunday as the first day of the first week (00..53)
+# %W - Week  number  of the current year, starting with the first Monday as the first day of the first week (00..53)
+# %w - Day of the week (Sunday is 0, 0..6)
+# %x - Preferred representation for the date alone, no time
+# %X - Preferred representation for the time alone, no date
+# %y - Year without a century (00..99)
+# %Y - Year with century
+# %Z - Time zone name
+# %% - Literal '%' character
+#
+# !!!NOTE!!!
+# The time zone field is ambiguous and cannot be parsed. For instance, CST is the abbreviation for China Standard Time, Central Standard Time, and Cuba Standard Time.
+#
+# The module documentation says that the strptime method is from FreeBSD, where the %Z format accepts either the local time zone or GMT and nothing else. This may be true of strptime, but I can confirm only that, where I am located, GMT is acceptable while UTC and AST are not.
+#
+# The solution I would recommend is to preprocess your time strings, replacing the time zone abbreviation with an unambiguous time zone offset. For instance AST (assuming you meant Atlantic Standard Time and not Arabia Standard Time) would be replaced with -0400, since it is four hours behind UTC. Then you can parse it with a %z format specifier and get the correct result.
+
 sub from_strptime ( $self, $date, $format ) {
     state $init = !!require Time::Piece;
 
-    if ( my $tp = Time::Piece->strptime( $date, $format ) ) {
-        return $self->from_object($tp);
-    }
+    return eval {
+        local $SIG{__DIE__} = undef;
 
-    return;
+        local $SIG{__WARN__} = sub { };
+
+        if ( my $tp = Time::Piece->strptime( $date, $format ) ) {
+            return $self->from_object($tp);
+        }
+    };
 }
 
 sub parse ( $self, $date ) {
@@ -55,6 +93,16 @@ sub to_w3cdtf ($self) {
 }
 
 1;
+## -----SOURCE FILTER LOG BEGIN-----
+##
+## PerlCritic profile "pcore-script" policy violations:
+## ┌──────┬──────────────────────┬────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
+## │ Sev. │ Lines                │ Policy                                                                                                         │
+## ╞══════╪══════════════════════╪════════════════════════════════════════════════════════════════════════════════════════════════════════════════╡
+## │    3 │ 43                   │ ErrorHandling::RequireCheckingReturnValueOfEval - Return value of eval not tested                              │
+## └──────┴──────────────────────┴────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
+##
+## -----SOURCE FILTER LOG END-----
 __END__
 =pod
 
