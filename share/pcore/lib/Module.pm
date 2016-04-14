be87b07985738cc294e1737f3b226014637233c3
zdm <zdm@softvisio.net>
2016-04-15 00:22:39 +0300

    default dist version is v0.0.0

--- share/pcore/lib/Module.pm
+++ share/pcore/lib/Module.pm
@@ -1,8 +1,18 @@
-package <: $module_name :> v0.1.0;
+package <: $module_name :> v0.0.0;
 
 use Pcore -dist;
 
 1;
+## -----SOURCE FILTER LOG BEGIN-----
+##
+## PerlCritic profile "pcore-script" policy violations:
+## ┌──────┬──────────────────────┬────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
+## │ Sev. │ Lines                │ Policy                                                                                                         │
+## ╞══════╪══════════════════════╪════════════════════════════════════════════════════════════════════════════════════════════════════════════════╡
+## │    1 │ 7                    │ Documentation::RequirePackageMatchesPodName - Pod NAME on line 11 does not match the package declaration       │
+## └──────┴──────────────────────┴────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
+##
+## -----SOURCE FILTER LOG END-----
 __END__
 =pod
 
