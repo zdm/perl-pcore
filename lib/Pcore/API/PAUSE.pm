2466c63f71a48dcf70af2379ff2a87ae5374a431
zdm <zdm@softvisio.net>
2016-06-21 14:30:52 +0300

    http api controller

--- lib/Pcore/API/PAUSE.pm
+++ lib/Pcore/API/PAUSE.pm
@@ -1,7 +1,6 @@
 package Pcore::API::PAUSE;
 
 use Pcore -class;
-use Pcore::API;
 use Pcore::Util::Text qw[encode_utf8];
 
 has username => ( is => 'ro', isa => Str, required => 1 );
@@ -123,11 +122,11 @@ sub _pack_multipart ( $self, $body, $boundary, $name, $content, $filename = unde
 ## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
 ## | Sev. | Lines                | Policy                                                                                                         |
 ## |======+======================+================================================================================================================|
-## |    3 | 57                   | RegularExpressions::ProhibitComplexRegexes - Split long regexps into smaller qr// chunks                       |
+## |    3 | 56                   | RegularExpressions::ProhibitComplexRegexes - Split long regexps into smaller qr// chunks                       |
 ## |------+----------------------+----------------------------------------------------------------------------------------------------------------|
-## |    3 | 68, 69, 73           | References::ProhibitDoubleSigils - Double-sigil dereference                                                    |
+## |    3 | 67, 68, 72           | References::ProhibitDoubleSigils - Double-sigil dereference                                                    |
 ## |------+----------------------+----------------------------------------------------------------------------------------------------------------|
-## |    3 | 103                  | Subroutines::ProhibitManyArgs - Too many arguments                                                             |
+## |    3 | 102                  | Subroutines::ProhibitManyArgs - Too many arguments                                                             |
 ## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
 ##
 ## -----SOURCE FILTER LOG END-----
---
+++ lib/Pcore/HTTP/Server/Controller/API.pm
@@ -0,0 +1,32 @@
+package Pcore::HTTP::Server::Controller::API;
+
+use Pcore -role;
+
+with qw[Pcore::HTTP::Server::Controller];
+
+# TODO get auth from token, then from cookies
+sub run ($self) {
+    return;
+}
+
+1;
+__END__
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Pcore::HTTP::Server::Controller::API
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 ATTRIBUTES
+
+=head1 METHODS
+
+=head1 SEE ALSO
+
+=cut
