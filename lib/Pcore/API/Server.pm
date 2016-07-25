f53c40e8cbc5561f03e1ebc9d54fb9190c141fb2
zdm <zdm@softvisio.net>
2016-07-25 17:38:38 +0300

    api auth class

--- lib/Pcore/API/Server.pm
+++ lib/Pcore/API/Server.pm
@@ -3,12 +3,16 @@ package Pcore::API::Server;
 use Pcore -class;
 use Pcore::API::Response;
 use Pcore::API::Server::Hash;
+use Pcore::Util::Hash::RandKey;
+use Pcore::API::Server::Auth;
 
 has namespace       => ( is => 'ro', isa => Str,         required => 1 );
 has default_version => ( is => 'ro', isa => PositiveInt, required => 1 );
 
+has dbh => ( is => 'lazy', isa => ConsumerOf ['Pcore::DBH'], init_arg => undef );
 has map => ( is => 'ro', isa => HashRef, init_arg => undef );
-has '_hash_rpc' => ( is => 'lazy', isa => InstanceOf ['Pcore::Util::PM::RPC'], init_arg => undef );
+has _hash_rpc => ( is => 'lazy', isa => InstanceOf ['Pcore::Util::PM::RPC'], init_arg => undef );
+has _hash_cache => ( is => 'ro', isa => InstanceOf ['Pcore::Util::Hash::RandKey'], default => sub { Pcore::Util::Hash::RandKey->new }, init_arg => undef );
 
 # TODO scan API classes
 sub BUILD ( $self, $args ) {
@@ -83,6 +87,36 @@ sub BUILD ( $self, $args ) {
     return;
 }
 
+sub _build_dbh ($self) {
+    my $dbh = P->handle('sqlite:auth.sqlite');
+
+    # create db
+    my $ddl = $dbh->ddl;
+
+    $ddl->add_changeset(
+        id  => 1,
+        sql => <<"SQL"
+            CREATE TABLE IF NOT EXISTS `user` (
+                `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                `username` TEXT NOT NULL UNIQUE,
+                `password` TEXT NOT NULL,
+                `enabled` INTEGER NOT NULL
+            );
+
+            CREATE TABLE IF NOT EXISTS `token` (
+                `id` BLOB PRIMARY KEY NOT NULL,
+                `token` TEXT NOT NULL,
+                `uid` INTEGER NOT NULL,
+                `enabled` INTEGER NOT NULL
+            );
+SQL
+    );
+
+    $ddl->upgrade;
+
+    return $dbh;
+}
+
 sub _build__hash_rpc($self) {
     return P->pm->run_rpc(
         'Pcore::API::Server::Hash',
@@ -96,46 +130,90 @@ sub _build__hash_rpc($self) {
     );
 }
 
-# TODO
-sub api_call ( $self, $version, $class, $method, $data, $auth, $cb ) {
-    my $blocking_cv = defined wantarray ? AE::cv : undef;
+# TODO return authenticated api object on success
+sub auth_password ( $self, $username, $password, $cb ) {
+    state $q1 = $self->dbh->query('SELECT * FROM user WHERE username = ?');
 
-    $version //= $self->default_version;
+    if ( my $user = $q1->selectrow( [$username] ) ) {
+        $self->_verify_hash(
+            $password,
+            $user->{password},
+            sub ($match) {
+                if ($match) {
+                    $cb->( Pcore::API::Server::Auth->new( { uid => $user->{id}, auth => 1 } ) );
+                }
+                else {
+                    $cb->(undef);
+                }
 
-    my $on_finish = sub ( $status, $reason = undef, $result = undef ) {
-        my $api_res = Pcore::API::Response->new( { status => $status, defined $reason ? ( reason => $reason ) : () } );
-
-        $api_res->{result} = $result;
-
-        $cb->($api_res) if $cb;
-
-        $blocking_cv->($api_res) if $blocking_cv;
-
-        return;
-    };
-
-    my $map = $self->{map}->{$version}->{$class};
-
-    if ( !$map ) {
-        $on_finish->( 404, q[API class was not found] );
-    }
-    elsif ( !exists $map->{method}->{$method} ) {
-        $on_finish->( 404, q[API method was not found] );
+                return;
+            }
+        );
     }
     else {
-
-        # TODO check auth
-        if (0) {
-            $on_finish->( 401, q[Unauthorized] );
-        }
-        else {
-            my $obj = bless { api => $self }, $map->{class};
-
-            $obj->$method( $data, $on_finish );
-        }
+        $cb->(undef);
     }
 
-    return defined $blocking_cv ? $blocking_cv->recv : ();
+    return;
+}
+
+# TODO check b64 decode and length
+# TODO return authenticated api object on success
+sub auth_token ( $self, $token_b64, $cb ) {
+    state $q1 = $self->dbh->query('SELECT * FROM token WHERE id = ?');
+
+    # TODO check, that decoded
+    my $token_raw = P->data->from_b64_url($token_b64);
+
+    my $token_id = substr $token_raw, 0, 16, q[];
+
+    if ( my $token = $q1->selectrow( [$token_id] ) ) {
+        $self->_verify_hash(
+            $token_raw,
+            $token->{token},
+            sub ($match) {
+                if ($match) {
+                    $cb->( Pcore::API::Server::Auth->new( { uid => $token->{uid}, auth => 1 } ) );
+                }
+                else {
+                    $cb->(undef);
+                }
+
+                return;
+            }
+        );
+    }
+    else {
+        $cb->(undef);
+    }
+
+    return;
+}
+
+# TODO implement cache size
+sub _verify_hash ( $self, $str, $hash, $cb ) {
+    $str = P->text->encode_utf8($str);
+
+    my $id = $str . $hash;
+
+    if ( exists $self->{_hash_cache}->{$id} ) {
+        $cb->(1);
+    }
+    else {
+        $self->_hash_rpc->rpc_call(
+            'verify_scrypt',
+            [ $str, $hash ],
+            sub ($match) {
+                $self->{_hash_cache}->{$id} = undef;
+
+                $cb->($match);
+
+                return;
+            }
+        );
+    }
+
+    return;
 }
 
 1;
@@ -145,11 +223,9 @@ sub api_call ( $self, $version, $class, $method, $data, $auth, $cb ) {
 ## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
 ## | Sev. | Lines                | Policy                                                                                                         |
 ## |======+======================+================================================================================================================|
-## |    3 | 45                   | References::ProhibitDoubleSigils - Double-sigil dereference                                                    |
+## |    3 | 49                   | References::ProhibitDoubleSigils - Double-sigil dereference                                                    |
 ## |------+----------------------+----------------------------------------------------------------------------------------------------------------|
-## |    3 | 100                  | Subroutines::ProhibitManyArgs - Too many arguments                                                             |
-## |------+----------------------+----------------------------------------------------------------------------------------------------------------|
-## |    2 | 34                   | ValuesAndExpressions::ProhibitNoisyQuotes - Quotes used with a noisy string                                    |
+## |    2 | 38                   | ValuesAndExpressions::ProhibitNoisyQuotes - Quotes used with a noisy string                                    |
 ## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
 ##
 ## -----SOURCE FILTER LOG END-----
---
+++ lib/Pcore/API/Server/Auth.pm
@@ -0,0 +1,149 @@
+package Pcore::API::Server::Auth;
+
+use Pcore -class;
+
+has uid  => ( is => 'ro', isa => PositiveInt, required => 1 );
+has auth => ( is => 'ro', isa => PositiveInt, required => 1 );
+
+sub is_root ($self) {
+    return $self->{uid} == 1;
+}
+
+sub api_call ( $self, $version, $class, $method, $data, $auth, $cb ) {
+    my $blocking_cv = defined wantarray ? AE::cv : undef;
+
+    $version //= $self->default_version;
+
+    my $on_finish = sub ( $status, $reason = undef, $result = undef ) {
+        my $api_res = Pcore::API::Response->new( { status => $status, defined $reason ? ( reason => $reason ) : () } );
+
+        $api_res->{result} = $result;
+
+        $cb->($api_res) if $cb;
+
+        $blocking_cv->($api_res) if $blocking_cv;
+
+        return;
+    };
+
+    my $map = $self->{map}->{$version}->{$class};
+
+    if ( !$map ) {
+        $on_finish->( 404, q[API class was not found] );
+    }
+    elsif ( !exists $map->{method}->{$method} ) {
+        $on_finish->( 404, q[API method was not found] );
+    }
+    else {
+
+        # TODO check auth
+        if (0) {
+            $on_finish->( 401, q[Unauthorized] );
+        }
+        else {
+            my $obj = bless { api => $self }, $map->{class};
+
+            $obj->$method( $data, $on_finish );
+        }
+    }
+
+    return defined $blocking_cv ? $blocking_cv->recv : ();
+}
+
+sub create_user ( $self, $username, $password, $cb ) {
+    state $q1 = $self->dbh->query('INSERT OR IGNORE INTO user (username, password, enabled) VALUES (?, 1, 0)');
+
+    state $q2 = $self->dbh->query('UPDATE user SET password = ?, enabled = 1 WHERE id = ?');
+
+    if ( $q1->do( [$username] ) ) {
+        my $id = $self->dbh->last_insert_id;
+
+        $self->_hash_rpc->rpc_call(
+            'create_scrypt',
+            [$password],
+            sub ($password_hash) {
+                $q2->do( [ $password_hash, $id ] );
+
+                $cb->($id);
+
+                return;
+            }
+        );
+    }
+    else {
+
+        # user already exists
+        $cb->(undef);
+    }
+
+    return;
+}
+
+# TODO store uid inside token
+sub create_token ( $self, $uid, $perms, $cb ) {
+    state $q1 = $self->dbh->query('INSERT OR IGNORE INTO token (id, token, uid, enabled) VALUES (?, ?, ?, 1)');
+
+    state $generate_token;
+
+    $generate_token //= sub ( $uid, $cb ) {
+        my $token_id = P->random->bytes(32);
+
+        my $token = substr $token_id, 0, 16, q[];
+
+        $self->_hash_rpc->rpc_call(
+            'create_scrypt',
+            [$token],
+            sub ($token_hash) {
+                if ( !$q1->do( [ $token_id, $token_hash, $uid ] ) ) {
+
+                    # token is not uniq.
+                    $generate_token->( $uid, $cb );
+
+                }
+                else {
+                    $cb->( P->data->to_b64_url( $token_id . $token ) );
+                }
+
+                return;
+            }
+        );
+
+        return;
+    };
+
+    $generate_token->( $uid, $cb );
+
+    return;
+}
+
+1;
+## -----SOURCE FILTER LOG BEGIN-----
+##
+## PerlCritic profile "pcore-script" policy violations:
+## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
+## | Sev. | Lines                | Policy                                                                                                         |
+## |======+======================+================================================================================================================|
+## |    3 | 12                   | Subroutines::ProhibitManyArgs - Too many arguments                                                             |
+## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
+##
+## -----SOURCE FILTER LOG END-----
+__END__
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Pcore::API::Server::Auth
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 ATTRIBUTES
+
+=head1 METHODS
+
+=head1 SEE ALSO
+
+=cut
