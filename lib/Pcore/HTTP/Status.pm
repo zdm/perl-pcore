93849e829eee9a6e6e08064e3b9fe0142f6ac18e
zdm <zdm@softvisio.net>
2016-08-21 20:59:51 +0300

    HTTP statuses updated

--- lib/Pcore/HTTP/Status.pm
+++ lib/Pcore/HTTP/Status.pm
@@ -17,82 +17,82 @@ use overload    #
 has status => ( is => 'ro', isa => PositiveInt, writer => 'set_status', default => 200 );
 has reason => ( is => 'ro', isa => Str );
 
-# stolen from HTTP::Status
+# http://www.iana.org/assignments/http-status-codes/http-status-codes.xhtml
 const our $STATUS_REASON => {
 
     # 100
-    100 => 'Continue',
-    101 => 'Switching Protocols',
-    102 => 'Processing',            # RFC 2518 (WebDAV)
+    '1xx' => 'Informational',
+    100   => 'Continue',
+    101   => 'Switching Protocols',
+    102   => 'Processing',
 
     # 200
-    200 => 'OK',
-    201 => 'Created',
-    202 => 'Accepted',
-    203 => 'Non-Authoritative Information',
-    204 => 'No Content',
-    205 => 'Reset Content',
-    206 => 'Partial Content',
-    207 => 'Multi-Status',                    # RFC 2518 (WebDAV)
-    208 => 'Already Reported',                # RFC 5842
-    226 => 'IM Used',
+    '2xx' => 'Success',
+    200   => 'OK',
+    201   => 'Created',
+    202   => 'Accepted',
+    203   => 'Non-Authoritative Information',
+    204   => 'No Content',
+    205   => 'Reset Content',
+    206   => 'Partial Content',
+    207   => 'Multi-Status',
+    208   => 'Already Reported',
+    226   => 'IM Used',
 
     # 300
-    300 => 'Multiple Choices',
-    301 => 'Moved Permanently',
-    302 => 'Found',
-    303 => 'See Other',
-    304 => 'Not Modified',
-    305 => 'Use Proxy',
-    306 => 'Switch Proxy',
-    307 => 'Temporary Redirect',
-    308 => 'Permanent Redirect',              # RFC 7238
+    '3xx' => 'Redirection',
+    300   => 'Multiple Choices',
+    301   => 'Moved Permanently',
+    302   => 'Found',
+    303   => 'See Other',
+    304   => 'Not Modified',
+    305   => 'Use Proxy',
+    307   => 'Temporary Redirect',
+    308   => 'Permanent Redirect',
 
     # 400
-    400 => 'Bad Request',
-    401 => 'Unauthorized',
-    402 => 'Payment Required',
-    403 => 'Forbidden',
-    404 => 'Not Found',
-    405 => 'Method Not Allowed',
-    406 => 'Not Acceptable',
-    407 => 'Proxy Authentication Required',
-    408 => 'Request Timeout',
-    409 => 'Conflict',
-    410 => 'Gone',
-    411 => 'Length Required',
-    412 => 'Precondition Failed',
-    413 => 'Payload Too Large',
-    414 => 'URI Too Long',
-    415 => 'Unsupported Media Type',
-    416 => 'Range Not Satisfiable',
-    417 => 'Expectation Failed',
-    418 => q[I'm a teapot],                     # RFC 2324
-    421 => 'Misdirected Request',
-    422 => 'Unprocessable Entity',              # RFC 2518 (WebDAV)
-    423 => 'Locked',                            # RFC 2518 (WebDAV)
-    424 => 'Failed Dependency',                 # RFC 2518 (WebDAV)
-    425 => 'No code',                           # WebDAV Advanced Collections
-    426 => 'Upgrade Required',                  # RFC 2817
-    428 => 'Precondition Required',
-    429 => 'Too Many Requests',
-    431 => 'Request Header Fields Too Large',
-    449 => 'Retry with',                        # unofficial Microsoft
-    451 => 'Unavailable For Legal Reasons',
+    '4xx' => 'Client Error',
+    400   => 'Bad Request',
+    401   => 'Unauthorized',
+    402   => 'Payment Required',
+    403   => 'Forbidden',
+    404   => 'Not Found',
+    405   => 'Method Not Allowed',
+    406   => 'Not Acceptable',
+    407   => 'Proxy Authentication Required',
+    408   => 'Request Timeout',
+    409   => 'Conflict',
+    410   => 'Gone',
+    411   => 'Length Required',
+    412   => 'Precondition Failed',
+    413   => 'Payload Too Large',
+    414   => 'URI Too Long',
+    415   => 'Unsupported Media Type',
+    416   => 'Range Not Satisfiable',
+    417   => 'Expectation Failed',
+    421   => 'Misdirected Request',
+    422   => 'Unprocessable Entity',
+    423   => 'Locked',
+    424   => 'Failed Dependency',
+    426   => 'Upgrade Required',
+    428   => 'Precondition Required',
+    429   => 'Too Many Requests',
+    431   => 'Request Header Fields Too Large',
+    451   => 'Unavailable For Legal Reasons',
 
     # 500
-    500 => 'Internal Server Error',
-    501 => 'Not Implemented',
-    502 => 'Bad Gateway',
-    503 => 'Service Unavailable',
-    504 => 'Gateway Timeout',
-    505 => 'HTTP Version Not Supported',
-    506 => 'Variant Also Negotiates',           # RFC 2295
-    507 => 'Insufficient Storage',              # RFC 2518 (WebDAV)
-    508 => 'Loop Detected',
-    509 => 'Bandwidth Limit Exceeded',          # unofficial
-    510 => 'Not Extended',                      # RFC 2774
-    511 => 'Network Authentication Required',
+    '5xx' => 'Server Error',
+    500   => 'Internal Server Error',
+    501   => 'Not Implemented',
+    502   => 'Bad Gateway',
+    503   => 'Service Unavailable',
+    504   => 'Gateway Timeout',
+    505   => 'HTTP Version Not Supported',
+    506   => 'Variant Also Negotiates',
+    507   => 'Insufficient Storage',
+    508   => 'Loop Detected',
+    510   => 'Not Extended',
+    511   => 'Network Authentication Required',
 };
 
 # COMMON REASON BUILDER
@@ -101,20 +101,20 @@ around reason => sub ( $orig, $self ) {
 
     if    ( exists $self->{reason} )           { return $self->{reason} }
     elsif ( exists $STATUS_REASON->{$status} ) { return $STATUS_REASON->{$status} }
-    elsif ( $status >= 100 && $status < 200 ) { return 'Informational' }
-    elsif ( $status >= 200 && $status < 300 ) { return 'Success' }
-    elsif ( $status >= 300 && $status < 400 ) { return 'Redirection' }
-    elsif ( $status >= 400 && $status < 500 ) { return 'Client Error' }
-    else                                      { return 'Error' }
+    elsif ( $status >= 100 && $status < 200 ) { return $STATUS_REASON->{'1xx'} }
+    elsif ( $status >= 200 && $status < 300 ) { return $STATUS_REASON->{'2xx'} }
+    elsif ( $status >= 300 && $status < 400 ) { return $STATUS_REASON->{'3xx'} }
+    elsif ( $status >= 400 && $status < 500 ) { return $STATUS_REASON->{'4xx'} }
+    else                                      { return $STATUS_REASON->{'5xx'} }
 };
 
 sub get_reason ( $self, $status ) {
     if ( exists $STATUS_REASON->{$status} ) { return $STATUS_REASON->{$status} }
-    elsif ( $status >= 100 && $status < 200 ) { return 'Informational' }
-    elsif ( $status >= 200 && $status < 300 ) { return 'Success' }
-    elsif ( $status >= 300 && $status < 400 ) { return 'Redirection' }
-    elsif ( $status >= 400 && $status < 500 ) { return 'Client Error' }
-    else                                      { return 'Error' }
+    elsif ( $status >= 100 && $status < 200 ) { return $STATUS_REASON->{'1xx'} }
+    elsif ( $status >= 200 && $status < 300 ) { return $STATUS_REASON->{'2xx'} }
+    elsif ( $status >= 300 && $status < 400 ) { return $STATUS_REASON->{'3xx'} }
+    elsif ( $status >= 400 && $status < 500 ) { return $STATUS_REASON->{'4xx'} }
+    else                                      { return $STATUS_REASON->{'5xx'} }
 }
 
 # STATUS WRITER
@@ -141,7 +141,7 @@ sub is_redirect ($self) {
 }
 
 sub is_error ($self) {
-    return $self->status >= 400 && $self->status < 600;
+    return $self->status >= 400;
 }
 
 sub is_client_error ($self) {
@@ -149,7 +149,7 @@ sub is_client_error ($self) {
 }
 
 sub is_server_error ($self) {
-    return $self->status >= 500 && $self->status < 600;
+    return $self->status >= 500;
 }
 
 1;
