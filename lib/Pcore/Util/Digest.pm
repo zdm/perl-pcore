af5928dc91143b346f02aa159a27a93dc4517b90
zdm <zdm@softvisio.net>
2016-09-11 17:20:49 +0300

    sha1_b64 method added

--- lib/Pcore/Util/Digest.pm
+++ lib/Pcore/Util/Digest.pm
@@ -3,7 +3,7 @@ package Pcore::Util::Digest;
 use Pcore -export => {
     CRC       => [qw[crc32]],
     MD5       => [qw[md5 md5_hex]],
-    SHA1      => [qw[sha1 sha1_hex]],
+    SHA1      => [qw[sha1 sha1_hex sha1_b64]],
     HMAC_SHA1 => [qw[hmac_sha1 hmac_sha1_hex]],
     SHA3      => [qw[sha3_224 sha3_224_hex sha3_256 sha3_256_hex sha3_384 sha3_384_hex sha3_512 sha3_512_hex]],
 };
@@ -13,6 +13,8 @@ use Digest::SHA qw[hmac_sha1 hmac_sha1_hex];
 use Digest::SHA3 qw[sha3_224 sha3_224_hex sha3_256 sha3_256_hex sha3_384 sha3_384_hex sha3_512 sha3_512_hex];
 
 my $BCRYPT_COST_DEFAULT = 10;
+
+*sha1_b64 = \&Digest::SHA1::sha1_base64;
 
 sub crc32 {
     state $init = !!require String::CRC32;
