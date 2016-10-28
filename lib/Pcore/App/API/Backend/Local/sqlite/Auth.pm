0e449dfa9a0a57c408a2645e7da7f7787eadf6a2
zdm <zdm@softvisio.net>
2016-10-28 11:42:01 +0300

    api user token

--- lib/Pcore/App/API/Backend/Local/sqlite/Auth.pm
+++ lib/Pcore/App/API/Backend/Local/sqlite/Auth.pm
@@ -102,111 +102,6 @@ SQL
     return;
 }
 
-sub _auth_user_token ( $self, $source_app_instance_id, $user_token_id, $private_token, $cb ) {
-    state $sql1 = <<'SQL';
-        SELECT
-            api_user.id AS user_id,
-            api_user.name AS user_name,
-            api_user.enabled AS user_enabled,
-            api_user_token.hash,
-            api_user_token.enabled AS user_token_enabled
-        FROM
-            api_user,
-            api_user_token
-        WHERE
-            api_user_token.id = ?
-            AND api_user_token.user_id = api_user.id
-SQL
-
-    state $sql2 = <<'SQL';
-        SELECT
-            api_app_role.name AS source_app_role_name
-        FROM
-            api_app_instance,
-            api_app_role,
-            api_user_permissions,
-            api_user_token_permissions
-        WHERE
-            api_app_instance.id = ?                                                         --- source app_instance_id
-            AND api_app_role.app_id = api_app_instance.app_id                               --- link source_app_instance_role to source_app
-            AND api_app_role.enabled = 1                                                    --- source_app_role must be enabled
-
-            AND api_app_role.id = api_user_permissions.role_id                              --- link app_role to user_permissions
-            AND api_user_permissions.enabled = 1                                            --- user permission must be enabled
-
-            AND api_user_permissions.id = api_user_token_permissions.user_permissions_id    --- link user_token_permissions to user_permissions
-            AND api_user_token_permissions.user_token_id = ?                                --- link user_token_permissions to user_token
-SQL
-
-    # get user token by token id
-    my $res = $self->dbh->selectrow( $sql1, [$user_token_id] );
-
-    # user token not found
-    if ( !$res ) {
-        $cb->( status [ 404, 'User token not found' ] );
-
-        return;
-    }
-
-    my $user_id = $res->{user_id};
-
-    my $continue = sub {
-        my $auth = {
-            user_id       => $user_id,
-            user_name     => $res->{user_name},
-            user_token_id => $user_token_id,
-            enabled       => $res->{user_enabled} && $res->{user_token_enabled},
-        };
-
-        my $tags = {
-            user_id       => $user_id,
-            user_token_id => $user_token_id,
-        };
-
-        # get permissions
-        if ( my $roles = $self->dbh->selectall( $sql2, [ $source_app_instance_id, $user_token_id ] ) ) {
-            for my $row ( $roles->@* ) {
-                $auth->{permissions}->{ $row->{source_app_role_name} } = 1;
-            }
-        }
-        else {
-            $auth->{permissions} = {};
-        }
-
-        $cb->( status 200, auth => $auth, tags => $tags );
-
-        return;
-    };
-
-    if ($private_token) {
-
-        # verify token
-        $self->_verify_token_hash(
-            $private_token . $user_id,
-            $res->{hash},
-            sub ($status) {
-
-                # token valid
-                if ($status) {
-                    $continue->();
-                }
-
-                # token is invalid
-                else {
-                    $cb->($status);
-                }
-
-                return;
-            }
-        );
-    }
-    else {
-        $continue->();
-    }
-
-    return;
-}
-
 # TODO
 sub _auth_user_session ( $self, $source_app_instance_id, $user_token_id, $private_token, $cb ) {
     state $sql1 = <<'SQL';
@@ -320,12 +215,11 @@ SQL
 ## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
 ## | Sev. | Lines                | Policy                                                                                                         |
 ## |======+======================+================================================================================================================|
-## |    3 | 5, 105, 211          | Subroutines::ProhibitManyArgs - Too many arguments                                                             |
+## |    3 | 5, 106               | Subroutines::ProhibitManyArgs - Too many arguments                                                             |
 ## |------+----------------------+----------------------------------------------------------------------------------------------------------------|
 ## |    3 |                      | Subroutines::ProhibitUnusedPrivateSubroutines                                                                  |
 ## |      | 5                    | * Private subroutine/method '_auth_app_instance_token' declared but not used                                   |
-## |      | 105                  | * Private subroutine/method '_auth_user_token' declared but not used                                           |
-## |      | 211                  | * Private subroutine/method '_auth_user_session' declared but not used                                         |
+## |      | 106                  | * Private subroutine/method '_auth_user_session' declared but not used                                         |
 ## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
 ##
 ## -----SOURCE FILTER LOG END-----
--- lib/Pcore/App/API/Backend/Local/sqlite/UserToken.pm
+++ lib/Pcore/App/API/Backend/Local/sqlite/UserToken.pm
@@ -2,6 +2,111 @@ package Pcore::App::API::Backend::Local::sqlite::UserToken;
 
 use Pcore -role, -promise, -status;
 
+sub _auth_user_token ( $self, $source_app_instance_id, $user_token_id, $private_token, $cb ) {
+    state $sql1 = <<'SQL';
+        SELECT
+            api_user.id AS user_id,
+            api_user.name AS user_name,
+            api_user.enabled AS user_enabled,
+            api_user_token.hash,
+            api_user_token.enabled AS user_token_enabled
+        FROM
+            api_user,
+            api_user_token
+        WHERE
+            api_user_token.id = ?
+            AND api_user_token.user_id = api_user.id
+SQL
+
+    state $sql2 = <<'SQL';
+        SELECT
+            api_app_role.name AS source_app_role_name
+        FROM
+            api_app_instance,
+            api_app_role,
+            api_user_permissions,
+            api_user_token_permissions
+        WHERE
+            api_app_instance.id = ?                                                         --- source app_instance_id
+            AND api_app_role.app_id = api_app_instance.app_id                               --- link source_app_instance_role to source_app
+            AND api_app_role.enabled = 1                                                    --- source_app_role must be enabled
+
+            AND api_app_role.id = api_user_permissions.role_id                              --- link app_role to user_permissions
+            AND api_user_permissions.enabled = 1                                            --- user permission must be enabled
+
+            AND api_user_permissions.id = api_user_token_permissions.user_permissions_id    --- link user_token_permissions to user_permissions
+            AND api_user_token_permissions.user_token_id = ?                                --- link user_token_permissions to user_token
+SQL
+
+    # get user token by token id
+    my $res = $self->dbh->selectrow( $sql1, [$user_token_id] );
+
+    # user token not found
+    if ( !$res ) {
+        $cb->( status [ 404, 'User token not found' ] );
+
+        return;
+    }
+
+    my $user_id = $res->{user_id};
+
+    my $continue = sub {
+        my $auth = {
+            user_id       => $user_id,
+            user_name     => $res->{user_name},
+            user_token_id => $user_token_id,
+            enabled       => $res->{user_enabled} && $res->{user_token_enabled},
+        };
+
+        my $tags = {
+            user_id       => $user_id,
+            user_token_id => $user_token_id,
+        };
+
+        # get permissions
+        if ( my $roles = $self->dbh->selectall( $sql2, [ $source_app_instance_id, $user_token_id ] ) ) {
+            for my $row ( $roles->@* ) {
+                $auth->{permissions}->{ $row->{source_app_role_name} } = 1;
+            }
+        }
+        else {
+            $auth->{permissions} = {};
+        }
+
+        $cb->( status 200, auth => $auth, tags => $tags );
+
+        return;
+    };
+
+    if ($private_token) {
+
+        # verify token
+        $self->_verify_token_hash(
+            $private_token . $user_id,
+            $res->{hash},
+            sub ($status) {
+
+                # token valid
+                if ($status) {
+                    $continue->();
+                }
+
+                # token is invalid
+                else {
+                    $cb->($status);
+                }
+
+                return;
+            }
+        );
+    }
+    else {
+        $continue->();
+    }
+
+    return;
+}
+
 sub get_user_token ( $self, $user_token_id, $cb ) {
     if ( my $user_token = $self->dbh->selectrow( q[SELECT * FROM api_user_token WHERE id = ?], [$user_token_id] ) ) {
         delete $user_token->{hash};
@@ -159,38 +264,6 @@ sub create_user_token ( $self, $user_id, $desc, $permissions, $cb ) {
     return;
 }
 
-sub set_user_token_enabled ( $self, $user_token_id, $enabled, $cb ) {
-    $self->get_user_token(
-        $user_token_id,
-        sub ( $res ) {
-            if ( !$res ) {
-                $cb->($res);
-            }
-            else {
-                my $user_token = $res->{user_token};
-
-                if ( ( $enabled && !$user_token->{enabled} ) || ( !$enabled && $user_token->{enabled} ) ) {
-                    if ( $self->dbh->do( q[UPDATE OR IGNORE api_user_token SET enabled = ? WHERE id = ?], [ !!$enabled, $user_token->{id} ] ) ) {
-                        $cb->( status 200 );
-                    }
-                    else {
-                        $cb->( status [ 500, 'Error set user token enabled' ] );
-                    }
-                }
-                else {
-
-                    # not modified
-                    $cb->( status 304 );
-                }
-            }
-
-            return;
-        }
-    );
-
-    return;
-}
-
 sub remove_user_token ( $self, $user_token_id, $cb ) {
     if ( $self->dbh->do( q[DELETE OR IGNORE FROM api_user_token WHERE id = ?], [$user_token_id] ) ) {
         $cb->( status 200 );
@@ -209,9 +282,12 @@ sub remove_user_token ( $self, $user_token_id, $cb ) {
 ## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
 ## | Sev. | Lines                | Policy                                                                                                         |
 ## |======+======================+================================================================================================================|
-## |    3 | 20, 162              | Subroutines::ProhibitManyArgs - Too many arguments                                                             |
+## |    3 | 5, 125               | Subroutines::ProhibitManyArgs - Too many arguments                                                             |
 ## |------+----------------------+----------------------------------------------------------------------------------------------------------------|
-## |    2 | 23                   | RegularExpressions::ProhibitFixedStringMatches - Use 'eq' or hash instead of fixed-pattern regexps             |
+## |    3 | 5                    | Subroutines::ProhibitUnusedPrivateSubroutines - Private subroutine/method '_auth_user_token' declared but not  |
+## |      |                      | used                                                                                                           |
+## |------+----------------------+----------------------------------------------------------------------------------------------------------------|
+## |    2 | 128                  | RegularExpressions::ProhibitFixedStringMatches - Use 'eq' or hash instead of fixed-pattern regexps             |
 ## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
 ##
 ## -----SOURCE FILTER LOG END-----
