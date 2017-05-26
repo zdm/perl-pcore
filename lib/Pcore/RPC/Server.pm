b08db8eedb3cb89ba04d6b77b2a3a7669e1f107f
zdm <zdm@softvisio.net>
2017-05-27 00:58:55 +0300

    websocket improvements

--- lib/Pcore/RPC/Server.pm
+++ lib/Pcore/RPC/Server.pm
@@ -70,7 +70,7 @@ sub run ( $class, $RPC_BOOT_ARGS ) {
         no strict qw[refs];
 
         if ( ${"$class\::RPC_LISTEN_EVENTS"} ) {
-            push $listen_events->@*, ref ${"$class\::RPC_LISTEN_EVENTS"} eq 'ARRAY' ? ${"${class}::RPC_LISTEN_EVENTS"}->@* : ${"$class\::RPC_LISTEN_EVENTS"};
+            push $listen_events->@*, ref ${"$class\::RPC_LISTEN_EVENTS"} eq 'ARRAY' ? ${"$class\::RPC_LISTEN_EVENTS"}->@* : ${"$class\::RPC_LISTEN_EVENTS"};
         }
     }
 
