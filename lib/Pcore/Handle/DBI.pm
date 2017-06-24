d4c927d035f0b635d5efc870c5297d36e47274db
zdm <zdm@softvisio.net>
2017-06-24 14:58:37 +0300

    dbi query parser

--- lib/Pcore/Handle/DBI.pm
+++ lib/Pcore/Handle/DBI.pm
@@ -3,6 +3,7 @@ package Pcore::Handle::DBI;
 use Pcore -role, -result, -const, -export => { TYPES => [qw[$SQL_ALL_TYPES $SQL_ARRAY $SQL_ARRAY_LOCATOR $SQL_BIGINT $SQL_BINARY $SQL_BIT $SQL_BLOB $SQL_BLOB_LOCATOR $SQL_BOOLEAN $SQL_CHAR $SQL_CLOB $SQL_CLOB_LOCATOR $SQL_DATE $SQL_DATETIME $SQL_DECIMAL $SQL_DOUBLE $SQL_FLOAT $SQL_GUID $SQL_INTEGER $SQL_INTERVAL $SQL_INTERVAL_DAY $SQL_INTERVAL_DAY_TO_HOUR $SQL_INTERVAL_DAY_TO_MINUTE $SQL_INTERVAL_DAY_TO_SECOND $SQL_INTERVAL_HOUR $SQL_INTERVAL_HOUR_TO_MINUTE $SQL_INTERVAL_HOUR_TO_SECOND $SQL_INTERVAL_MINUTE $SQL_INTERVAL_MINUTE_TO_SECOND $SQL_INTERVAL_MONTH $SQL_INTERVAL_SECOND $SQL_INTERVAL_YEAR $SQL_INTERVAL_YEAR_TO_MONTH $SQL_LONGVARBINARY $SQL_LONGVARCHAR $SQL_MULTISET $SQL_MULTISET_LOCATOR $SQL_NUMERIC $SQL_REAL $SQL_REF $SQL_ROW $SQL_SMALLINT $SQL_TIME $SQL_TIMESTAMP $SQL_TINYINT $SQL_TYPE_DATE $SQL_TYPE_TIME $SQL_TYPE_TIMESTAMP $SQL_TYPE_TIMESTAMP_WITH_TIMEZONE $SQL_TYPE_TIME_WITH_TIMEZONE $SQL_UDT $SQL_UDT_LOCATOR $SQL_UNKNOWN_TYPE $SQL_VARBINARY $SQL_VARCHAR $SQL_WCHAR $SQL_WLONGVARCHAR $SQL_WVARCHAR]], };
 use Pcore::Handle::DBI::STH;
 use Pcore::Util::UUID qw[uuid_str];
+use Pcore::Util::Scalar qw[is_ref is_plain_scalarref is_plain_hashref is_plain_arrayref];
 
 with qw[Pcore::Handle];
 
@@ -149,11 +150,83 @@ sub prepare ( $self, $query ) {
     return $sth;
 }
 
-# TODO return $query, $bind
+# TODO "VALUES" context
 sub prepare_query ( $self, $query ) {
-    my ( $sql, $bind );
+    state $context_re = do {
+        my @keywords = qw[SET VALUES WHERE];
 
-    ...;
+        my $context_keywords_prepared = join q[|], sort { length $b <=> length $a } map {s/\s+/\\s+/smgr} @keywords;
+
+        qr/(?:(?<=\A)|(?<=\s))(?:$context_keywords_prepared)(?=\s|\z)/smi;
+    };
+
+    my ( $sql, $bind, $i, $last_not_ref, $context );
+
+    for my $arg ( $query->@* ) {
+        if ( !is_ref $arg ) {
+            die q[SQL query builder doesn't allow several consecutive non-ref argument] if $last_not_ref;
+
+            $last_not_ref = 1;
+
+            # trim
+            $sql .= ' ' . $arg =~ s/\A\s+|\s+\z//smgr;
+
+            # analyse context
+            if ( my $last_kw = ( $arg =~ /$context_re/smgi )[-1] ) {
+                $context = uc $last_kw =~ s/\s+/ /smgr;
+            }
+        }
+        else {
+            $last_not_ref = 0;
+
+            if ( is_plain_scalarref $arg) {
+                $sql .= ' $' . ++$i;
+
+                push $bind->@*, $arg->$*;
+            }
+            else {
+                if ( $context eq 'SET' ) {
+                    my @fields;
+
+                    for my $field ( sort keys $arg->%* ) {
+                        push @fields, $self->quote_id($field) . ' = $' . ++$i;
+
+                        push $bind->@*, $arg->{$field};
+                    }
+
+                    $sql .= ' ' . join q[, ], @fields;
+                }
+                elsif ( $context eq 'WHERE' ) {
+                    if ( is_plain_hashref $arg) {
+                        my @fields;
+
+                        for my $field ( keys $arg->%* ) {
+                            push @fields, $self->quote_id($field) . ' = $' . ++$i;
+
+                            push $bind->@*, $arg->{$field};
+                        }
+
+                        $sql .= ' (' . join( ' AND ', @fields ) . ')';
+                    }
+                    elsif ( is_plain_arrayref $arg) {
+                        $sql .= ' (' . join( ', ', map { '$' . ++$i } $arg->@* ) . ')';
+
+                        push $bind->@*, $arg->@*;
+                    }
+                    else {
+                        die q[SQL "WHERE" context support only HashRef or ArrayReh arguments];
+                    }
+                }
+                elsif ( $context eq 'VALUES' ) {
+
+                    # TODO
+                }
+                else {
+                    die 'Unknown SQL context';
+                }
+            }
+        }
+    }
 
     return $sql, $bind;
 }
@@ -331,11 +404,15 @@ PERL
 ## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
 ## | Sev. | Lines                | Policy                                                                                                         |
 ## |======+======================+================================================================================================================|
-## |    3 | 83                   | Subroutines::ProhibitUnusedPrivateSubroutines - Private subroutine/method '_get_dbh' declared but not used     |
+## |    3 | 84                   | Subroutines::ProhibitUnusedPrivateSubroutines - Private subroutine/method '_get_dbh' declared but not used     |
 ## |------+----------------------+----------------------------------------------------------------------------------------------------------------|
-## |    3 | 156                  | ControlStructures::ProhibitYadaOperator - yada operator (...) used                                             |
+## |    3 | 203                  | ControlStructures::ProhibitDeepNests - Code structure is deeply nested                                         |
 ## |------+----------------------+----------------------------------------------------------------------------------------------------------------|
-## |    3 | 281, 307             | ErrorHandling::RequireCheckingReturnValueOfEval - Return value of eval not tested                              |
+## |    3 | 354, 380             | ErrorHandling::RequireCheckingReturnValueOfEval - Return value of eval not tested                              |
+## |------+----------------------+----------------------------------------------------------------------------------------------------------------|
+## |    2 | 172, 197             | ValuesAndExpressions::ProhibitEmptyQuotes - Quotes used with a string containing no non-whitespace characters  |
+## |------+----------------------+----------------------------------------------------------------------------------------------------------------|
+## |    1 | 158                  | BuiltinFunctions::ProhibitReverseSortBlock - Forbid $b before $a in sort blocks                                |
 ## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
 ##
 ## -----SOURCE FILTER LOG END-----
