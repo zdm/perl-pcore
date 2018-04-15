e33382faf8a0b6624fa8d962f7063e09264f97ff
zdm <zdm@softvisio.net>
2018-04-16 00:23:31 +0300

    proxy pool removed

--- lib/Pcore/API/Proxy.pm
+++ lib/Pcore/API/Proxy.pm
@@ -1,6 +1,6 @@
 package Pcore::API::Proxy;
 
-use Pcore -class, -res;
+use Pcore -const, -class, -res, -export => { PROXY_TYPE => [qw[$PROXY_TYPE_HTTP $PROXY_TYPE_CONNECT $PROXY_TYPE_SOCKS]] };
 use Pcore::Util::Scalar qw[is_ref];
 
 has uri => ( is => 'ro', isa => Str | InstanceOf ['Pcore::Util::URI'], required => 1 );
@@ -9,15 +9,9 @@ has pool => ( is => 'ro', isa => Maybe [Object] );
 
 has threads => ( is => 'ro', isa => PositiveOrZeroInt, default => 0, init_arg => undef );
 
-sub DEMOLISH ( $self, $global ) {
-    if ( !$global ) {
-        $self->{threads}--;
-
-        $self->{pool}->push_proxy($self) if defined $self->{pool};
-    }
-
-    return;
-}
+const our $PROXY_TYPE_HTTP    => 1;
+const our $PROXY_TYPE_CONNECT => 2;
+const our $PROXY_TYPE_SOCKS   => 3;
 
 around new => sub ( $orig, $self, $uri ) {
     $uri = P->uri($uri) if !is_ref $uri;
@@ -25,8 +19,28 @@ around new => sub ( $orig, $self, $uri ) {
     return $self->$orig( { uri => $uri } );
 };
 
-sub connect_http ( $self, $target, @args ) {
+sub connect ( $self, $uri, @args ) {    ## no critic qw[Subroutines::ProhibitBuiltinHomonyms]
+    $uri = P->uri($uri) if !is_ref $uri;
+
+    if ( $uri->is_http ) {
+        if ( $uri->is_secure ) {
+            return $self->connect_https( $uri, @args );
+        }
+        else {
+            return $self->connect_http( $uri, @args );
+        }
+    }
+    else {
+        return $self->connect_socks( $uri, @args );
+    }
+}
+
+sub connect_http ( $self, $uri, @args ) {
     my $cb = pop @args;
+
+    $uri = P->uri($uri) if !is_ref $uri;
+
+    $self->start_thread;
 
     Pcore::AE::Handle->new(
         connect => $self->{uri},
@@ -38,12 +52,17 @@ sub connect_http ( $self, $target, @args ) {
         # bind_ip          => $args->{bind_ip},
 
         on_connect_error => sub ( $h, $reason ) {
+            $self->finish_thread;
+
             $cb->( undef, res [ 600, $reason ] );
 
             return;
         },
         on_connect => sub ( $h, $host, $port, $retry ) {
-            $self->{threads}++;
+            $h->{proxy}      = $self;
+            $h->{proxy_type} = $PROXY_TYPE_HTTP;
+
+            $h->starttls('connect') if $self->{uri}->is_secure;
 
             $cb->( $h, res 200 );
 
@@ -54,8 +73,12 @@ sub connect_http ( $self, $target, @args ) {
     return;
 }
 
-sub connect_https ( $self, $target, @args ) {
+sub connect_https ( $self, $uri, @args ) {
     my $cb = pop @args;
+
+    $uri = P->uri($uri) if !is_ref $uri;
+
+    $self->start_thread;
 
     Pcore::AE::Handle->new(
         connect => $self->{uri},
@@ -67,12 +90,16 @@ sub connect_https ( $self, $target, @args ) {
         # bind_ip          => $args->{bind_ip},
 
         on_connect_error => sub ( $h, $reason ) {
+            $self->finish_thread;
+
             $cb->( undef, res [ 600, $reason ] );
 
             return;
         },
         on_connect => sub ( $h, $host, $port, $retry ) {
-            my $buf = 'CONNECT ' . $target->hostport . q[ HTTP/1.1] . $CRLF;
+            $h->starttls('connect') if $self->{uri}->is_secure;
+
+            my $buf = 'CONNECT ' . $uri->hostport . q[ HTTP/1.1] . $CRLF;
 
             $buf .= 'Proxy-Authorization: Basic ' . $self->{uri}->userinfo_b64 . $CRLF if $self->{uri}->userinfo;
 
@@ -84,20 +111,21 @@ sub connect_https ( $self, $target, @args ) {
                 headers => 0,
                 sub ( $h1, $res, $error_reason ) {
                     if ($error_reason) {
+                        $self->finish_thread;
+
                         $cb->( undef, res [ 600, 'Invalid proxy connect response' ] );
                     }
                     else {
                         if ( $res->{status} == 200 ) {
-                            $h->{peername} = $target->host;
-
-                            $self->{threads}++;
+                            $h->{proxy}      = $self;
+                            $h->{proxy_type} = $PROXY_TYPE_CONNECT;
+                            $h->{peername}   = $uri->host;
 
                             $cb->( $h, res 200 );
                         }
-                        elsif ( $res->{status} == 407 ) {
-                            $cb->( undef, res [ 407, $res->{reason} ] );
-                        }
                         else {
+                            $self->finish_thread;
+
                             $cb->( undef, res [ $res->{status}, $res->{reason} ] );
                         }
                     }
@@ -112,6 +140,221 @@ sub connect_https ( $self, $target, @args ) {
 
     return;
 }
+
+sub connect_socks ( $self, $uri, @args ) {
+    $uri = P->uri($uri) if !is_ref $uri;
+
+    return;
+}
+
+sub start_thread ($self) {
+    $self->{threads}++;
+
+    $self->{pool}->start_thread($self) if defined $self->{pool};
+
+    return;
+}
+
+sub finish_thread ($self) {
+    $self->{threads}--;
+
+    $self->{pool}->finish_thread($self) if defined $self->{pool};
+
+    return;
+}
+
+# ----------------------------------------------------------
+
+# sub _connect_proxy_socks4 ( $self, $proxy, $connect, $on_finish ) {
+#     AnyEvent::Socket::resolve_sockaddr $connect->[0], $connect->[1], 'tcp', undef, undef, sub {
+#         my @target = @_;
+#
+#         unless (@target) {
+#             $on_finish->( $self, qq[Host name "$connect->[0]" couldn't be resolved], $PROXY_ERROR_OTHER );    # not a proxy connect error
+#
+#             return;
+#         }
+#
+#         my $target = shift @target;
+#
+#         $self->push_write( qq[\x04\x01] . pack( 'n', $connect->[1] ) . AnyEvent::Socket::unpack_sockaddr( $target->[3] ) . $proxy->userinfo . qq[\x00] );
+#
+#         $self->unshift_read(
+#             chunk => 8,
+#             sub ( $h, $chunk ) {
+#                 my $rep = unpack 'C*', substr( $chunk, 1, 1 );
+#
+#                 # request granted
+#                 if ( $rep == 90 ) {
+#                     $on_finish->( $h, undef, undef );
+#                 }
+#
+#                 # request rejected or failed, tunnel creation error
+#                 elsif ( $rep == 91 ) {
+#                     $on_finish->( $h, 'Request rejected or failed', $PROXY_ERROR_OTHER );
+#                 }
+#
+#                 # request rejected becasue SOCKS server cannot connect to identd on the client
+#                 elsif ( $rep == 92 ) {
+#                     $on_finish->( $h, 'Request rejected becasue SOCKS server cannot connect to identd on the client', $PROXY_ERROR_AUTH );
+#                 }
+#
+#                 # request rejected because the client program and identd report different user-ids
+#                 elsif ( $rep == 93 ) {
+#                     $on_finish->( $h, 'Request rejected because the client program and identd report different user-ids', $PROXY_ERROR_AUTH );
+#                 }
+#
+#                 # unknown error or not SOCKS4 proxy response
+#                 else {
+#                     $on_finish->( $h, 'Invalid socks4 server response', $PROXY_ERROR_OTHER );
+#                 }
+#
+#                 return;
+#             }
+#         );
+#
+#         return;
+#     };
+#
+#     return;
+# }
+#
+# sub _connect_proxy_socks5 ( $self, $proxy, $connect, $on_finish ) {
+#
+#     # start handshake
+#     # no authentication or authenticate with username/password
+#     if ( $proxy->userinfo ) {
+#         $self->push_write(qq[\x05\x02\x00\x02]);
+#     }
+#
+#     # no authentication
+#     else {
+#         $self->push_write(qq[\x05\x01\x00]);
+#     }
+#
+#     $self->unshift_read(
+#         chunk => 2,
+#         sub ( $h, $chunk ) {
+#             my ( $ver, $auth_method ) = unpack 'C*', $chunk;
+#
+#             # no valid authentication method was proposed
+#             if ( $auth_method == 255 ) {
+#                 $on_finish->( $h, 'No authentication method was found', $PROXY_ERROR_AUTH );
+#             }
+#
+#             # start username / password authentication
+#             elsif ( $auth_method == 2 ) {
+#
+#                 # send authentication credentials
+#                 $h->push_write( qq[\x01] . pack( 'C', length $proxy->username ) . $proxy->username . pack( 'C', length $proxy->password ) . $proxy->password );
+#
+#                 # read authentication response
+#                 $h->unshift_read(
+#                     chunk => 2,
+#                     sub ( $h, $chunk ) {
+#                         my ( $auth_ver, $auth_status ) = unpack 'C*', $chunk;
+#
+#                         # authentication error
+#                         if ( $auth_status != 0 ) {
+#                             $on_finish->( $h, 'Authentication failure', $PROXY_ERROR_AUTH );
+#                         }
+#
+#                         # authenticated
+#                         else {
+#                             _socks5_establish_tunnel( $h, $proxy, $connect, $on_finish );
+#                         }
+#
+#                         return;
+#                     }
+#                 );
+#             }
+#
+#             # no authentication is needed
+#             elsif ( $auth_method == 0 ) {
+#                 _socks5_establish_tunnel( $h, $proxy, $connect, $on_finish );
+#
+#                 return;
+#             }
+#
+#             # unknown authentication method or not SOCKS5 response
+#             else {
+#                 $on_finish->( $h, 'Authentication method is not supported', $PROXY_ERROR_OTHER );
+#             }
+#
+#             return;
+#         }
+#     );
+#
+#     return;
+# }
+#
+# sub _socks5_establish_tunnel ( $self, $proxy, $connect, $on_finish ) {
+#
+#     # detect destination addr type
+#     if ( my $ipn4 = AnyEvent::Socket::parse_ipv4( $connect->[0] ) ) {    # IPv4 addr
+#         $self->push_write( qq[\x05\x01\x00\x01] . $ipn4 . pack( 'n', $connect->[1] ) );
+#     }
+#     elsif ( my $ipn6 = AnyEvent::Socket::parse_ipv6( $connect->[0] ) ) {    # IPv6 addr
+#         $self->push_write( qq[\x05\x01\x00\x04] . $ipn6 . pack( 'n', $connect->[1] ) );
+#     }
+#     else {                                                                  # domain name
+#         $self->push_write( qq[\x05\x01\x00\x03] . pack( 'C', length $connect->[0] ) . $connect->[0] . pack( 'n', $connect->[1] ) );
+#     }
+#
+#     $self->unshift_read(
+#         chunk => 4,
+#         sub ( $h, $chunk ) {
+#             my ( $ver, $rep, $rsv, $atyp ) = unpack( 'C*', $chunk );
+#
+#             if ( $rep == 0 ) {
+#                 if ( $atyp == 1 ) {                                         # IPv4 addr, 4 bytes
+#                     $h->unshift_read(                                       # read IPv4 addr (4 bytes) + port (2 bytes)
+#                         chunk => 6,
+#                         sub ( $h, $chunk ) {
+#                             $on_finish->( $h, undef, undef );
+#
+#                             return;
+#                         }
+#                     );
+#                 }
+#                 elsif ( $atyp == 3 ) {                                      # domain name
+#                     $h->unshift_read(                                       # read domain name length
+#                         chunk => 1,
+#                         sub ( $h, $chunk ) {
+#                             $h->unshift_read(                               # read domain name + port (2 bytes)
+#                                 chunk => unpack( 'C', $chunk ) + 2,
+#                                 sub ( $h, $chunk ) {
+#                                     $on_finish->( $h, undef, undef );
+#
+#                                     return;
+#                                 }
+#                             );
+#
+#                             return;
+#                         }
+#                     );
+#                 }
+#                 if ( $atyp == 4 ) {    # IPv6 addr, 16 bytes
+#                     $h->unshift_read(    # read IPv6 addr (16 bytes) + port (2 bytes)
+#                         chunk => 18,
+#                         sub ( $h, $chunk ) {
+#                             $on_finish->( $h, undef, undef );
+#
+#                             return;
+#                         }
+#                     );
+#                 }
+#             }
+#             else {
+#                 $on_finish->( $h, q[Tunnel creation error], $PROXY_ERROR_OTHER );
+#             }
+#
+#             return;
+#         }
+#     );
+#
+#     return;
+# }
 
 1;
 __END__
--- lib/Pcore/HTTP/Util.pm
+++ lib/Pcore/HTTP/Util.pm
@@ -2,7 +2,7 @@ package Pcore::HTTP::Util;
 
 use Pcore -const;
 use Errno qw[];
-use Pcore::AE::Handle2 qw[:PROXY_TYPE];
+use Pcore::API::Proxy qw[:PROXY_TYPE];
 use Pcore::AE::Handle;
 use Pcore::Util::Scalar qw[is_ref is_plain_coderef];
 use Compress::Raw::Zlib qw[WANT_GZIP_OR_ZLIB Z_OK Z_STREAM_END];
@@ -198,80 +198,25 @@ sub http_request ($args) {
 }
 
 sub _connect ( $args, $runtime, $cb ) {
-
-    # if ( $args->{proxy} ) {
-    #     Pcore::AE::Handle2->new(
-    #         $args->{handle_params}->%*,
-    #         connect                => $args->{url},
-    #         connect_timeout        => $args->{connect_timeout},
-    #         timeout                => $args->{timeout},
-    #         tls_ctx                => $args->{tls_ctx},
-    #         bind_ip                => $args->{bind_ip},
-    #         proxy                  => $args->{proxy},
-    #         on_proxy_connect_error => sub ( $h, $reason, $proxy_error ) {
-    #             $runtime->{finish}->( 594, $reason );
-    #
-    #             return;
-    #         },
-    #         on_connect_error => sub ( $h, $reason ) {
-    #             $runtime->{finish}->( $runtime->{on_error_status}, $reason );
-    #
-    #             return;
-    #         },
-    #         on_error => sub ( $h, $fatal, $reason ) {
-    #             $runtime->{finish}->( $runtime->{on_error_status}, $reason );
-    #
-    #             return;
-    #         },
-    #         on_connect => sub ( $h, $host, $port, $retry ) {
-    #             if ( $h->{proxy} && $h->{proxy_type} && $h->{proxy_type} == $PROXY_TYPE_HTTP && $h->{proxy}->userinfo ) {
-    #                 $args->{headers}->{PROXY_AUTHORIZATION} = 'Basic ' . $h->{proxy}->userinfo_b64;
-    #             }
-    #             else {
-    #                 delete $args->{headers}->{PROXY_AUTHORIZATION};
-    #             }
-    #
-    #             $cb->($h);
-    #
-    #             return;
-    #         },
-    #     );
-    # }
     if ( $args->{proxy} ) {
-        if ( !$args->{url}->is_secure ) {
-            $args->{proxy}->connect_http(
-                $args->{url},
-                tls_ctx => $args->{tls_ctx},
-                sub ( $h, $res ) {
-                    if ( !$res ) {
-                        $runtime->{finish}->( $runtime->{on_error_status}, $res->{reason} );
-                    }
-                    else {
-                        $args->{headers}->{PROXY_AUTHORIZATION} = 'Basic ' . $args->{proxy}->{uri}->userinfo_b64 if $args->{proxy}->{uri}->userinfo_b64;
+        $args->{proxy} = Pcore::API::Proxy->new( $args->{proxy} ) if !is_ref $args->{proxy};
 
-                        $cb->($h);
-                    }
-
-                    return;
+        $args->{proxy}->connect(
+            $args->{url},
+            timeout => $args->{timeout},
+            tls_ctx => $args->{tls_ctx},
+            bind_ip => $args->{bind_ip},
+            sub ( $h, $res ) {
+                if ( !$res ) {
+                    $runtime->{finish}->( $runtime->{on_error_status}, $res->{reason} );
                 }
-            );
-        }
-        else {
-            $args->{proxy}->connect_https(
-                $args->{url},
-                tls_ctx => $args->{tls_ctx},
-                sub ( $h, $res ) {
-                    if ( !$res ) {
-                        $runtime->{finish}->( $runtime->{on_error_status}, $res->{reason} );
-                    }
-                    else {
-                        $cb->($h);
-                    }
-
-                    return;
+                else {
+                    $cb->($h);
                 }
-            );
-        }
+
+                return;
+            }
+        );
     }
     else {
         Pcore::AE::Handle->new(
@@ -293,8 +238,6 @@ sub _connect ( $args, $runtime, $cb ) {
                 return;
             },
             on_connect => sub ( $h, $host, $port, $retry ) {
-                delete $args->{headers}->{PROXY_AUTHORIZATION};
-
                 $cb->($h);
 
                 return;
@@ -308,7 +251,7 @@ sub _connect ( $args, $runtime, $cb ) {
 sub _write_request ( $args, $runtime ) {
     my $request_path;
 
-    if ( $args->{proxy} && !$args->{url}->is_secure ) {    # proxy is HTTP
+    if ( $runtime->{h}->{proxy_type} && $runtime->{h}->{proxy_type} == $PROXY_TYPE_HTTP ) {
         $request_path = $args->{url}->to_string;
     }
     else {
@@ -346,6 +289,11 @@ sub _write_request ( $args, $runtime ) {
     # close connection, if not persistent
     if ( !$args->{persistent} && !$args->{headers}->{CONNECTION} ) {
         $headers .= 'Connection:close' . $CRLF;
+    }
+
+    # delete $args->{headers}->{PROXY_AUTHORIZATION};
+    if ( $runtime->{h}->{proxy_type} && $runtime->{h}->{proxy_type} == $PROXY_TYPE_HTTP && $runtime->{h}->{proxy}->{uri}->userinfo ) {
+        $headers .= 'Proxy-Authorization:Basic ' . $runtime->{h}->{proxy}->{uri}->userinfo_b64 . $CRLF;
     }
 
     # send request headers
@@ -698,12 +646,12 @@ sub _read_body ( $args, $runtime, $cb ) {
 ## |======+======================+================================================================================================================|
 ## |    3 |                      | Subroutines::ProhibitExcessComplexity                                                                          |
 ## |      | 26                   | * Subroutine "http_request" with high complexity score (26)                                                    |
-## |      | 308                  | * Subroutine "_write_request" with high complexity score (25)                                                  |
-## |      | 427                  | * Subroutine "_read_body" with high complexity score (67)                                                      |
+## |      | 251                  | * Subroutine "_write_request" with high complexity score (28)                                                  |
+## |      | 375                  | * Subroutine "_read_body" with high complexity score (67)                                                      |
 ## |------+----------------------+----------------------------------------------------------------------------------------------------------------|
-## |    3 | 407                  | ErrorHandling::RequireCheckingReturnValueOfEval - Return value of eval not tested                              |
+## |    3 | 355                  | ErrorHandling::RequireCheckingReturnValueOfEval - Return value of eval not tested                              |
 ## |------+----------------------+----------------------------------------------------------------------------------------------------------------|
-## |    3 | 639                  | ControlStructures::ProhibitDeepNests - Code structure is deeply nested                                         |
+## |    3 | 587                  | ControlStructures::ProhibitDeepNests - Code structure is deeply nested                                         |
 ## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
 ##
 ## -----SOURCE FILTER LOG END-----
