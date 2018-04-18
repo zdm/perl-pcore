ca7af1c5613a573400280bbf55abe950d96d3a97
zdm <zdm@softvisio.net>
2018-04-18 08:52:24 +0300

    $ENV refactoring

--- lib/Pcore/Core/Env/Share.pm
+++ lib/Pcore/Core/Env/Share.pm
@@ -4,13 +4,13 @@ use Pcore -class, -const;
 use Pcore::Util::Scalar qw[is_plain_arrayref is_plain_hashref];
 
 has _temp        => ( is => 'lazy', isa => InstanceOf ['Pcore::Util::File::TempDir'], init_arg => undef );
-has _lib         => ( is => 'ro',   isa => HashRef,                                   init_arg => undef );                                          # name => [$level, $path]
-has _storage     => ( is => 'lazy', isa => HashRef,                                   default  => sub { {} }, clearer => 1, init_arg => undef );    # storage cache, name => [$path, ...]
-has _lib_storage => ( is => 'lazy', isa => HashRef,                                   default  => sub { {} }, init_arg => undef );                  # lib storage cache, {lib}->{storage} = $path
+has _lib         => ( is => 'ro',   isa => HashRef,                                   init_arg => undef );    # name => [$level, $path]
+has _storage     => ( is => 'ro',   isa => HashRef,                                   init_arg => undef );    # storage cache, name => [$path, ...]
+has _lib_storage => ( is => 'ro',   isa => HashRef,                                   init_arg => undef );    # lib storage cache, {lib}->{storage} = $path
 
 const our $RESERVED_LIB_NAME => {
-    dist => 1,                                                                                                                                      # alias for main dist
-    temp => 1,                                                                                                                                      # temporary resources lib
+    dist => 1,                                                                                                # alias for main dist
+    temp => 1,                                                                                                # temporary resources lib
 };
 
 sub _build__temp ($self) {
@@ -26,7 +26,7 @@ sub add_lib ( $self, $name, $path, $level ) {
     $self->{_lib}->{$name} = [ $level, $path ];
 
     # clear cache
-    $self->_clear_storage;
+    delete $self->{_storage};
 
     return;
 }
@@ -66,37 +66,34 @@ sub get_storage ( $self, $storage_name, $lib_name = undef ) {
 
         die qq[resource lib is not exists "$lib_name"] if !$lib_path;
 
-        \my $lib_storage = \$self->_lib_storage;
-
         # cache lib/storage path, if not cached yet
-        if ( !exists $lib_storage->{$lib_name}->{$storage_name} ) {
+        if ( !exists $self->{_lib_storage}->{$lib_name}->{$storage_name} ) {
             if ( -d "${lib_path}${storage_name}" ) {
-                $lib_storage->{$lib_name}->{$storage_name} = $lib_path . $storage_name;
+                $self->{_lib_storage}->{$lib_name}->{$storage_name} = $lib_path . $storage_name;
             }
             else {
-                $lib_storage->{$lib_name}->{$storage_name} = undef;
+                $self->{_lib_storage}->{$lib_name}->{$storage_name} = undef;
             }
         }
 
         # return cached path
-        return $lib_storage->{$lib_name}->{$storage_name};
+        return $self->{_lib_storage}->{$lib_name}->{$storage_name};
     }
     else {
-        \my $storage = \$self->_storage;
 
         # build and cache storage paths array
-        if ( !exists $storage->{$storage_name} ) {
+        if ( !exists $self->{_storage}->{$storage_name} ) {
             for my $lib_name ( sort { $libs->{$b}->[0] <=> $libs->{$a}->[0] } keys $libs->%* ) {
                 my $storage_path = $libs->{$lib_name}->[1] . $storage_name;
 
-                push $storage->{$storage_name}->@*, $storage_path if -d $storage_path;
+                push $self->{_storage}->{$storage_name}->@*, $storage_path if -d $storage_path;
             }
 
-            $storage->{$storage_name} = undef if !exists $storage->{$storage_name};
+            $self->{_storage}->{$storage_name} = undef if !exists $self->{_storage}->{$storage_name};
         }
 
         # return cached value
-        return $storage->{$storage_name};
+        return $self->{_storage}->{$storage_name};
     }
 }
 
@@ -167,9 +164,9 @@ sub store ( $self, $path, $file, $lib_name, @ ) {
 
     # clear storage cache if new storage was created
     if ( !-d "${lib_path}$args{storage}" ) {
-        delete $self->_storage->{ $args{storage} };
+        delete $self->{_storage}->{ $args{storage} };
 
-        delete $self->_lib_storage->{$lib_name}->{ $args{storage} } if exists $self->_lib_storage->{$lib_name};
+        delete $self->{_lib_storage}->{$lib_name}->{ $args{storage} } if exists $self->{_lib_storage}->{$lib_name};
     }
 
     # create path
@@ -196,9 +193,9 @@ sub store ( $self, $path, $file, $lib_name, @ ) {
 ## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
 ## | Sev. | Lines                | Policy                                                                                                         |
 ## |======+======================+================================================================================================================|
-## |    3 | 143                  | Subroutines::ProhibitManyArgs - Too many arguments                                                             |
+## |    3 | 140                  | Subroutines::ProhibitManyArgs - Too many arguments                                                             |
 ## |------+----------------------+----------------------------------------------------------------------------------------------------------------|
-## |    1 | 89                   | BuiltinFunctions::ProhibitReverseSortBlock - Forbid $b before $a in sort blocks                                |
+## |    1 | 86                   | BuiltinFunctions::ProhibitReverseSortBlock - Forbid $b before $a in sort blocks                                |
 ## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
 ##
 ## -----SOURCE FILTER LOG END-----
