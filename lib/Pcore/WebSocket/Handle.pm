e9c90a2cbcf08b03c0f6cd933d30b3f4fed319cd
zdm <zdm@softvisio.net>
2018-05-12 23:01:00 +0300

    reader attr property removed

--- lib/Pcore/WebSocket/Handle.pm
+++ lib/Pcore/WebSocket/Handle.pm
@@ -19,9 +19,9 @@ requires qw[protocol before_connect_server before_connect_client on_connect_serv
 has max_message_size => ( is => 'ro', isa => PositiveOrZeroInt, default => 1_024 * 1_024 * 100 );    # 0 - do not check message size
 has pong_interval    => ( is => 'ro', isa => PositiveOrZeroInt, default => 0 );                      # 0 - do not pong automatically
 has compression      => ( is => 'ro', isa => Bool,              default => 0 );                      # use permessage_deflate compression
-has on_disconnect => ( is => 'ro', isa => Maybe [CodeRef], reader => undef );                        # ($ws, $status)
-has on_ping       => ( is => 'ro', isa => Maybe [CodeRef], reader => undef );                        # ($ws, $status)
-has on_pong       => ( is => 'ro', isa => Maybe [CodeRef], reader => undef );                        # ($ws, $status)
+has on_disconnect => ( isa => Maybe [CodeRef] );                                                     # ($ws, $status)
+has on_ping       => ( isa => Maybe [CodeRef] );                                                     # ($ws, $status)
+has on_pong       => ( isa => Maybe [CodeRef] );                                                     # ($ws, $status)
 
 has h => ( is => 'ro', isa => InstanceOf ['Pcore::AE::Handle'], init_arg => undef );
 has is_connected => ( is => 'ro', isa => Bool, default => 0, init_arg => undef );
