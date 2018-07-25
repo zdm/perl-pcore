868ff6b7a3e10c7a6774124f0e8f79c7fe47e16c
zdm <zdm@softvisio.net>
2018-07-25 05:53:34 +0300

    Pcore::Handle updated

--- lib/Pcore/Handle.pm
+++ lib/Pcore/Handle.pm
@@ -1,15 +1,402 @@
 package Pcore::Handle;
 
-use Pcore -role;
+use Pcore -const, -class, -export;
+use Pcore::Util::CA;
+use Pcore::Util::Scalar qw[is_ref is_plain_arrayref is_plain_coderef is_glob is_plain_hashref is_blessed];
+use AnyEvent::Socket qw[];
+use Errno qw[EAGAIN EWOULDBLOCK EINTR];
+use AnyEvent::Util qw[WSAEWOULDBLOCK];
+use IO::Socket::SSL qw[$SSL_ERROR SSL_WANT_READ SSL_WANT_WRITE SSL_VERIFY_NONE SSL_VERIFY_PEER];
+use Coro::EV qw[];
+use overload    #
+  q[bool]  => sub { return substr( $_[0]->{status}, 0, 1 ) == 2 },
+  q[0+]    => sub { return $_[0]->{status} },
+  q[""]    => sub { return $_[0]->{status} . q[ ] . $_[0]->{reason} },
+  fallback => 1;
 
-has uri => ();    # ( is => 'ro', isa => InstanceOf ['Pcore::Util::URI'], required => 1 );
+our $EXPORT = { TLS_CTX => [qw[$TLS_CTX_HIGH $TLS_CTX_LOW]] };
 
-sub new ( $self, $uri, %args ) {
-    $uri = P->uri($uri) if !ref $uri;
+const our $TLS_CTX_LOW  => 0;
+const our $TLS_CTX_HIGH => 1;
+const our $TLS_CTX      => {
+    $TLS_CTX_LOW => {
+        $MSWIN ? ( SSL_ca_file => Pcore::Util::CA->ca_file ) : (),
+        SSL_verify_mode => SSL_VERIFY_NONE,
+        dh              => undef,
+    },
+    $TLS_CTX_HIGH => {
+        $MSWIN ? ( SSL_ca_file => Pcore::Util::CA->ca_file ) : (),
+        SSL_verify_mode => SSL_VERIFY_PEER,
+        dh              => 8192,
+    },
+};
 
-    my $class = P->class->load( $uri->scheme, ns => 'Pcore::Handle' );
+const our $DH_PARAM => {
+    2048 => q[MIIBCAKCAQEAhR5Fn9h3Tgnc+q4o3CMkZtre3lLUyDT+1bf3aiVOt22JdDQndZLc|FeKz8AqliB3UIgNExc6oDtuG4znKPgklfOnHv/a9tl1AYQbV+QFM/E0jYl6oG8tF|Epgxezt1GCivvtu64ql0s213wr64QffNMt3hva8lNqK1PXfqp13PzzLzAVsfghrv|fMAX7/bYm1T5fAJdcah6FeZkKof+mqbs8HtRjfvrUF2npEM2WdupFu190vcwABnN|TTJheXCWv2BF2f9EEr61q3OUhSNWIThtZP+NKe2bACm1PebT0drAcaxKoMz9LjKr|y5onGs0TOuQ7JmhtZL45Zr4LwBcyTucLUwIBAg==],
+    4096 => q[MIICCAKCAgEA5WwA5lQg09YRYqc/JILCd2AfBmYBkF19wmCEJB8G3JhTxv8EGvYk|xyP2ecKVUvHTG8Xw/qpW8nRqzPIyV8QRf6YFYSf33Qnx2xYhcnqOumU3nfC0SNOL|/w2q1BA9BbHtW4574P+6hOQx9ftRtbtZ2HPKBMRcAKGjpYZiKopv0+UAM4NpEC2p|bfajp7pyVLeb/Aqm/oWP3L63wPlY1SDp+XRzrOAKB+/uLGqEwV0bBaxxGL29BpOp|O2z1ALGXiDCcLs9WTn9WqUhWDzUN6fahm53rd7zxwpFCb6K2YhaK0peG95jzSUJ8|aoL0KgWuC6v5+gPJHRu0HrQIdfAdN4VchqYOKE46uNNkQl8VJGu4RjYB7lFBpRwO|g2HCsGMo2X7BRmA1st66fh+JOd1smXMZG/2ozTOooL+ixcx4spNneg4aQerWl5cb|nWXKtPCp8yPzt/zoNzL3Fon2Ses3sNgMos0M/ZbnigScDxz84Ms6V/X8Z0L4m/qX|mL42dP40tgvmgqi6BdsBzcIWeHlEcIhmGcsEBxxKEg7gjb0OjjvatpUCJhmRrGjJ|LtMkBR68qr42OBMN/PBB4KPOWNUqTauXZajfCwYdbpvV24ZhtkcRdw1zisyARBSh|aTKW/GV8iLsUzlYN27LgVEwMwnWQaoecW6eOTNKGUURC3In6XZSvVzsCAQI=],
+    8192 => q[MIIECAKCBAEA/SAEbRSSLenVxoInHiltm/ztSwehGOhOiUKfzDcKlRBZHlCC9jBl|S/aeklM6Ucg8E6J2bnfoh6CAdnE/glQOn6CifhZr8X/rnlL9/eP+r9m+aiAw4l0D|MBd8BondbEqwTZthMmLtx0SslnevsFAZ1Cj8WgmUNaSPOukvJ1N7aQ98U+E99Pw3|VG8ANBydXqLqW2sogS8FtZoMbVywcQuaGmC7M6i3Akxe3CCSIpR/JkEZIytREBSC|CH+x3oW/w+wHzq3w8DGB9hqz1iMXqDMiPIMSdXC0DaIPokLnd7X8u6N14yCAco2h|P0gspD3J8pS2FpUY8ZTVjzbVCjhNNmTryBZAxHSWBuX4xYcCHUtfGlUe/IGLSVE1|xIdFpZUfvlvAJjVq0/TtDMg3r2JSXrhQVlr8MPJwSApDVr5kOBHT/uABio4z+5yR|PAvundznfyo9GGAWhIA36GQqsxSQfoRTjWssFoR/cu+9aomRwwOLkvObu8nCVVLH|nLdKDk5cIR0TvNs9HZ6ZmkzL7ah7cPzEKl7U6eE6yZLVYMNecnPLS6PSAIG4gxcq|CVQrrZjQLfTDrJn0OGgpShX85RaDsuiRtp2bpDZ23YDqdwr4wRjvIargjqc2zcF+|jIb7dUS6ci7bVG/CGOQUuiMWAiXZ3a1f343SMf9A05/sf1xwnMeco6STBLZ3X+PA|4urU+grtpWaFtS/fPD2ILn8nrJ3WuSKKUeSnVM46mmJQsOkyn7z8l3jNLB17GYKo|qc+0UuU/2PM9qtZdZElSM/ACLV2vdCuaibop4B9UIP9z3F8kfZ72+zKxpGiE+Bo1|x8SfG8FQw90mYIx+qZzJ8MCvc2wh+l4wDX5KxrhwvcouE2tHQlwfDgv/DiIXp173|hAmUCV0+bPRW8IIJvBODdAWtJe9hNwxj1FFYmPA7l4wa3gXV4I6tb+iO1MbwVjZ/|116tD5MdCo3JuSisgPYCHfkQccwEO0FHEuBbmfN+fQimQ8H0dePP8XctwbkplsB+|aLT5hYKmva/j9smEswgyHglPwc3WvZ+2DgKk7A7DHi7a2gDwCRQlHaXtNWx3992R|dfNgkSeB1CvGSQoo95WpC9ZoqGmcSlVqdetDU8iglPmfYTKO8aIPA6TuTQ/lQ0IW|90LQmqP23FwnNFiyqX8+rztLq4KVkTyeHIQwig6vFxgD8N+SbZCW2PPiB72TVF2U|WePU8MRTv1OIGBUBajF49k28HnZPSGlILHtFEkYkbPvomcE5ENnoejwzjktOTS5d|/R3SIOvCauOzadtzwTYOXT78ORaR1KI1cm8DzkkwJTd/Rrk07Q5vnvnSJQMwFUeH|PwJIgWBQf/GZ/OsDHmkbYR2ZWDClbKw2mwIBAg==],
+};
 
-    return $class->new( { %args, uri => $uri } );    ## no critic qw[ValuesAndExpressions::ProhibitCommaSeparatedStatements]
+const our $HANDLE_STATUS_OK            => 200;
+const our $HANDLE_STATUS_TIMEOUT       => 201;
+const our $HANDLE_STATUS_CONNECT_ERROR => 590;
+const our $HANDLE_STATUS_TLS_ERROR     => 591;
+const our $HANDLE_STATUS_TIMEOUT_ERROR => 597;
+const our $HANDLE_STATUS_ERROR         => 598;
+const our $HANDLE_STATUS_EOF           => 599;
+
+const our $STATUS_REASON => {
+    $HANDLE_STATUS_OK            => 'OK',
+    $HANDLE_STATUS_TIMEOUT       => 'Timeout',
+    $HANDLE_STATUS_CONNECT_ERROR => 'Connect error',
+    $HANDLE_STATUS_TLS_ERROR     => 'TLS handshake error',
+    $HANDLE_STATUS_TIMEOUT_ERROR => 'Disconnected on timeout',
+    $HANDLE_STATUS_ERROR         => 'Disconnected',
+    $HANDLE_STATUS_EOF           => 'EOF',
+};
+
+our $SCHEME_CACHE = {};
+
+has fh               => ();
+has read_size        => 1024 * 1024;
+has timeout          => 30;
+has timeout_is_fatal => 1;
+has connect_timeout  => 10;
+has persistent       => ();
+has tls_ctx          => $TLS_CTX_HIGH;
+has bind_ip          => ();
+has peername         => ();
+has so_no_delay      => 1;
+has so_keepalive     => 1;
+has so_oobinline     => 1;
+
+has rbuf => ();
+has tls  => ();
+
+# TODO handle persistent
+sub DESTROY ($self) {
+    if ( ${^GLOBAL_PHASE} ne 'DESTRUCT' ) {
+
+    }
+
+    return;
+}
+
+# TODO persistent
+around new => sub ( $orig, $self, $connect, @args ) {
+
+    # wrap fh
+    if ( is_glob $connect) {
+        $self = $self->$orig(@args);
+
+        $self->{fh} = $connect;
+    }
+
+    # connect
+    else {
+        my ( $uri, $scheme );
+
+        # parse connect
+        if ( !is_ref $connect) {
+            $uri = P->uri( $connect, authority => 1 );
+            $scheme = $uri->scheme;
+
+            $connect = [ $uri->host, $uri->port || $uri->default_port ];
+        }
+        elsif ( !is_plain_arrayref $connect) {
+            $uri    = $connect;
+            $scheme = $connect->scheme;
+
+            $connect = [ $connect->host, $connect->port || $connect->default_port ];
+        }
+
+        if ($scheme) {
+            if ( !exists $SCHEME_CACHE->{$scheme} ) {
+                my $class = eval { P->class->load( $uri->scheme, ns => 'Pcore::Handle' ) };
+
+                $SCHEME_CACHE->{$scheme} = $class;
+            }
+
+            return $SCHEME_CACHE->{$scheme}->new( { @args, uri => $uri } ) if $SCHEME_CACHE->{$scheme};    ## no critic qw[ValuesAndExpressions::ProhibitCommaSeparatedStatements]
+        }
+
+        $self = $self->$orig(@args);
+
+        $self->{peername} //= $connect->[0];
+
+        my $bind_error;
+
+        my $rouse_cb = Coro::rouse_cb;
+
+        AnyEvent::Socket::tcp_connect(
+            $connect->[0],
+            $connect->[1],
+            sub ( $fh = undef, $host = undef, $port = undef, $retry = undef ) {
+                if ($fh) {
+                    if ($bind_error) {
+                        $self->_set_status( $HANDLE_STATUS_CONNECT_ERROR, qq[Unable to bind socket to the IP address $self->{bind_ip}] );
+                    }
+                    else {
+                        $self->_set_status($HANDLE_STATUS_OK);
+
+                        $self->{fh} = $fh;
+                    }
+                }
+                else {
+                    $self->_set_status( $HANDLE_STATUS_CONNECT_ERROR, $! );
+                }
+
+                $rouse_cb->();
+
+                return;
+            },
+            sub ($fh) {
+                bind $fh, Socket::pack_sockaddr_in( 0, Socket::inet_aton( $self->{bind_ip} ) ) or $bind_error = 1 if $self->{bind_ip};
+
+                return $self->{connect_timeout};
+            }
+        );
+
+        Coro::rouse_wait $rouse_cb;
+    }
+
+    # configure fh
+    if ( $self->{fh} ) {
+        AnyEvent::fh_unblock $self->{fh};
+
+        $self->so_no_delay( $self->{so_no_delay} );
+        $self->so_oobinline( $self->{so_oobinline} );
+        $self->so_keepalive( $self->{so_keepalive} );
+    }
+
+    return $self;
+};
+
+sub so_no_delay ( $self, $val ) {
+    $self->{so_no_delay} = $val;
+
+    setsockopt $self->{fh}, Socket::IPPROTO_TCP(), Socket::TCP_NODELAY(), int $val or die $!;
+
+    return;
+}
+
+sub so_oobinline ( $self, $val ) {
+    $self->{so_oobinline} = $val;
+
+    setsockopt $self->{fh}, Socket::SOL_SOCKET(), Socket::SO_OOBINLINE(), int $val or die $!;
+
+    return;
+}
+
+sub so_keepalive ( $self, $val ) {
+    $self->{so_keepalive} = $val;
+
+    setsockopt $self->{fh}, Socket::SOL_SOCKET(), Socket::SO_KEEPALIVE(), int $val or die $!;
+
+    return;
+}
+
+sub can_read ( $self, $timeout = undef ) {
+    my $res = Coro::EV::timed_io_once $self->{fh}, EV::READ, $timeout // $self->{timeout};
+
+    return 1 if $res == EV::READ;
+
+    $self->_set_status( $self->{timeout_is_fatal} ? $HANDLE_STATUS_TIMEOUT_ERROR : $HANDLE_STATUS_TIMEOUT ) if $res == EV::TIMER;
+
+    return;
+}
+
+sub can_write ( $self, $timeout = undef ) {
+    my $res = Coro::EV::timed_io_once $self->{fh}, EV::WRITE, $timeout // $self->{timeout};
+
+    return 1 if $res == EV::WRITE;
+
+    $self->_set_status( $self->{timeout_is_fatal} ? $HANDLE_STATUS_TIMEOUT_ERROR : $HANDLE_STATUS_TIMEOUT ) if $res == EV::TIMER;
+
+    return;
+}
+
+# TODO use ->pending in TLS mode and read_size < 16K
+# returns: undef or total bytes read
+sub _read ( $self, $timeout = undef ) {
+    return if !$self;
+
+    my $bytes;
+
+    while () {
+        $bytes = sysread $self->{fh}, $self->{rbuf}, $self->{read_size}, length $self->{rbuf} // 0;
+
+        if ( defined $bytes ) {
+
+            # EOF
+            $self->_set_status($HANDLE_STATUS_EOF) if $bytes == 0;
+
+            last;
+        }
+
+        # error
+        elsif ( $! != EAGAIN && $! != EINTR && $! != WSAEWOULDBLOCK && $! != EWOULDBLOCK ) {
+            $self->_set_status( $HANDLE_STATUS_ERROR, $! );
+
+            last;
+        }
+
+        $self->can_read($timeout) || last;
+    }
+
+    return $bytes;
+}
+
+# returns: undef or buffer
+sub read ( $self, $timeout = undef ) {    ## no critic qw[Subroutines::ProhibitBuiltinHomonyms]
+    return if !$self;
+
+    return delete $self->{rbuf} if length $self->{rbuf};
+
+    $self->_read($timeout);
+
+    return delete $self->{rbuf} if length $self->{rbuf};
+
+    return;
+}
+
+# returns: undef or buffer
+sub readline ( $self, $term, $timeout = undef ) {    ## no critic qw[Subroutines::ProhibitBuiltinHomonyms]
+    while () {
+        my $idx = defined $self->{rbuf} ? index $self->{rbuf}, $term, 0 : -1;
+
+        if ( $idx == 0 ) {
+            return substr $self->{rbuf}, 0, length $term, q[];
+        }
+        elsif ( $idx > 0 ) {
+            return substr $self->{rbuf}, 0, $idx + length $term, q[];
+        }
+
+        # pattern not found
+        else {
+            return if !$self;
+
+            $self->_read($timeout) || last;
+        }
+    }
+
+    return;
+}
+
+# returns: undef or buffer
+sub readchunk ( $self, $len, $timeout = undef ) {
+    while () {
+        if ( defined $self->{rbuf} ) {
+            if ( length $self->{rbuf} == $len ) {
+                return delete $self->{rbuf};
+            }
+            elsif ( length $self->{rbuf} > $len ) {
+                return substr $self->{rbuf}, 0, $len, q[];
+            }
+        }
+
+        return if !$self;
+
+        $self->_read($timeout) || last;
+    }
+
+    return;
+}
+
+# returns: undef or total bytes written
+sub write ( $self, $buf, $timeout = undef ) {    ## no critic qw[Subroutines::ProhibitBuiltinHomonyms]
+    return if !$self;
+
+    my $total_bytes;
+    my $size = length $buf;
+    my $ofs  = 0;
+
+    while () {
+        my $bytes = syswrite $self->{fh}, $buf, $size, $ofs;
+
+        if ( defined $bytes ) {
+            $total_bytes += $bytes;
+            $size -= $bytes;
+
+            # all data written
+            last if $size == 0;
+
+            $ofs += $bytes;
+
+            # repeat
+            next;
+        }
+
+        # error
+        elsif ( $! != EAGAIN && $! != EINTR && $! != WSAEWOULDBLOCK && $! != EWOULDBLOCK ) {
+            $self->_set_status( $HANDLE_STATUS_ERROR, $! );
+
+            last;
+        }
+
+        $self->can_write($timeout) || last;
+    }
+
+    return $total_bytes;
+}
+
+sub starttls ( $self, $timeout = undef ) {
+    die q[TLS is already started] if $self->{tls};
+
+    my %ctx = do {
+        if ( !defined $self->{tls_ctx} ) {
+            ();
+        }
+        elsif ( is_plain_hashref $self->{tls_ctx} ) {
+            $self->{tls_ctx}->%*;
+        }
+        else {
+            $TLS_CTX->{ $self->{tls_ctx} }->%*;
+        }
+    };
+
+    $ctx{SSL_startHandshake} = 0;
+    $ctx{SSL_hostname}       = $ctx{SSL_verifycn_name} = $self->{peername};
+    $ctx{SSL_dh}             = $DH_PARAM->{ delete $ctx{dh} } if $ctx{dh};
+
+    $self->{fh} = IO::Socket::SSL->start_SSL( $self->{fh}, %ctx );
+
+    $self->{tls} = 1;
+
+    while () {
+        $self->{fh}->connect_SSL && last;
+
+        if ( $! == EAGAIN || $! == EINTR || $! == WSAEWOULDBLOCK || $! == EWOULDBLOCK ) {
+            if ( $SSL_ERROR == SSL_WANT_READ ) {
+                $self->can_read($timeout) && next;
+            }
+            elsif ( $SSL_ERROR == SSL_WANT_WRITE ) {
+                $self->can_write($timeout) && next;
+            }
+        }
+
+        $self->_set_status($HANDLE_STATUS_TLS_ERROR);
+
+        last;
+    }
+
+    return;
+}
+
+# STATUS METHODS
+sub is_connect_error ($self) { return $self->{status} == $HANDLE_STATUS_CONNECT_ERROR }
+sub is_tls_error ($self)     { return $self->{status} == $HANDLE_STATUS_TLS_ERROR }
+sub is_eof ($self)           { return $self->{status} == $HANDLE_STATUS_EOF }
+sub is_timeoout ($self)      { return $self->{status} == $HANDLE_STATUS_TIMEOUT || $self->{status} == $HANDLE_STATUS_TIMEOUT_ERROR }
+
+sub _set_status ( $self, $status, $reason = undef ) {
+    $self->{status} = $status;
+
+    $self->{reason} = $reason // $STATUS_REASON->{$status};
+
+    delete $self->{fh} if substr( $status, 0, 1 ) != 2;
+
+    return;
 }
 
 1;
