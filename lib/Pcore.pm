97e70ca027aaa901320d669be83134d6ef068526
zdm <zdm@softvisio.net>
2018-08-28 15:36:08 +0300

    P::cv

--- lib/Pcore.pm
+++ lib/Pcore.pm
@@ -441,6 +441,86 @@ sub sendlog ( $self, $key, $title, $data = undef ) {
     return;
 }
 
+# CV
+sub cv : prototype(;&) ( $cb = undef ) { return bless [$cb], 'Pcore::cv' }
+
+*P::cv = \&cv;
+
+package Pcore::cv {    ## no critic qw[Modules::ProhibitMultiplePackages]
+    use common::header;
+    use Const::Fast qw[const];
+
+    const our $CB       => 0;
+    const our $IS_READY => 1;
+    const our $COUNTER  => 2;
+    const our $ROUSE_CB => 3;
+    const our $ARGS     => 4;
+
+    use overload       #
+      '&{}' => sub ( $self, @ ) {
+        return sub { return $self->send(@_) }
+      },
+      'bool' => sub ($self) { return $self->[$IS_READY] };
+
+    sub is_ready ($self) { return $self->[$IS_READY] }
+
+    sub begin ( $self, $cb = undef ) {
+        ++$self->[$COUNTER];
+
+        $self->[$CB] = $cb if @_ > 1;
+
+        return $self;
+    }
+
+    sub end ($self) {
+        return if --$self->[$COUNTER] > 0;
+
+        my $cb = $self->[$CB];
+
+        defined $cb ? $cb->($self) : $self->send();
+
+        return;
+    }
+
+    sub recv ($self) {    ## no critic qw[Subroutines::ProhibitBuiltinHomonyms]
+
+        # already finished
+        if ( $self->[$IS_READY] ) {
+            return if !defined $self->[$ARGS];
+
+            my $res = delete $self->[$ARGS];
+
+            return wantarray ? $res->@* : $res->[0];
+        }
+
+        return Coro::rouse_wait( $self->[$ROUSE_CB] //= Coro::rouse_cb() );
+    }
+
+    sub send {    ## no critic qw[Subroutines::ProhibitBuiltinHomonyms]
+        my $self = shift;
+
+        # already finished
+        return if $self->[$IS_READY];
+
+        # mark as finished
+        $self->[$IS_READY] = 1;
+
+        # has rouse_cb
+        if ( defined $self->[$ROUSE_CB] ) {
+
+            # call rouse_cb
+            delete( $self->[$ROUSE_CB] )->(@_);
+        }
+        else {
+
+            # store args
+            $self->[$ARGS] = [@_];
+        }
+
+        return;
+    }
+}
+
 1;
 ## -----SOURCE FILTER LOG BEGIN-----
 ##
