ad2b1f05916398426f114098b89a8f3db34b99c9
zdm <zdm@softvisio.net>
2018-08-29 19:39:01 +0300

    P::cv

--- lib/Pcore.pm
+++ lib/Pcore.pm
@@ -442,84 +442,11 @@ sub sendlog ( $self, $key, $title, $data = undef ) {
 }
 
 # CV
-sub cv ( $self, $cb = undef ) { return bless [$cb], 'Pcore::cv' }
+*cv = *P::cv = sub ( $self, $cb = undef ) {
+    state $init = !!require Pcore::Core::CV;
 
-*P::cv = \&cv;
-
-package Pcore::cv {    ## no critic qw[Modules::ProhibitMultiplePackages]
-    use common::header;
-    use Const::Fast qw[const];
-
-    const our $CB       => 0;
-    const our $IS_READY => 1;
-    const our $COUNTER  => 2;
-    const our $ROUSE_CB => 3;
-    const our $ARGS     => 4;
-
-    use overload       #
-      '&{}' => sub ( $self, @ ) {
-        return sub { return $self->send(@_) }
-      },
-      fallback => 1;
-
-    sub is_ready ($self) { return $self->[$IS_READY] }
-
-    sub begin ( $self, $cb = undef ) {
-        ++$self->[$COUNTER];
-
-        $self->[$CB] = $cb if @_ > 1;
-
-        return $self;
-    }
-
-    sub end ($self) {
-        return $self if --$self->[$COUNTER] > 0;
-
-        my $cb = $self->[$CB];
-
-        defined $cb ? $cb->($self) : $self->send();
-
-        return $self;
-    }
-
-    sub recv ($self) {    ## no critic qw[Subroutines::ProhibitBuiltinHomonyms]
-
-        # already finished
-        if ( $self->[$IS_READY] ) {
-            return if !defined $self->[$ARGS];
-
-            my $res = delete $self->[$ARGS];
-
-            return wantarray ? $res->@* : $res->[0];
-        }
-
-        return Coro::rouse_wait( $self->[$ROUSE_CB] //= Coro::rouse_cb() );
-    }
-
-    sub send {    ## no critic qw[Subroutines::ProhibitBuiltinHomonyms]
-        my $self = shift;
-
-        # already finished
-        return if $self->[$IS_READY];
-
-        # mark as finished
-        $self->[$IS_READY] = 1;
-
-        # has rouse_cb
-        if ( defined $self->[$ROUSE_CB] ) {
-
-            # call rouse_cb
-            delete( $self->[$ROUSE_CB] )->(@_);
-        }
-        else {
-
-            # store args
-            $self->[$ARGS] = [@_];
-        }
-
-        return;
-    }
-}
+    return bless [$cb], 'Pcore::Core::CV';
+};
 
 1;
 ## -----SOURCE FILTER LOG BEGIN-----
