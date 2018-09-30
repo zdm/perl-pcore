4a55e42de944623dad7bff51113323426e8efa26
zdm <zdm@softvisio.net>
2018-09-30 10:10:43 +0300

    path1 prototype

--- lib/Pcore/Util/Path1/Dir.pm
+++ lib/Pcore/Util/Path1/Dir.pm
@@ -1,6 +1,34 @@
 package Pcore::Util::Path1::Dir;
 
 use Pcore -role;
+use IO::AIO qw[];
+use Pcore::Util::Scalar qw[is_plain_coderef];
+
+sub read_dir ( $self, @ ) {
+    my $cb = is_plain_coderef $_[-1] ? shift : ();
+
+    my $cv = defined wantarray ? P->cv : ();
+
+    my %args = (
+        recursive => 0,
+        @_[ 1 .. $#_ ]
+    );
+
+    # IO::AIO::READDIR_DENTS
+    # IO::AIO::READDIR_DIRS_FIRST
+    # IO::AIO::READDIR_STAT_ORDER
+    my $flags = IO::AIO::READDIR_DENTS | IO::AIO::READDIR_DIRS_FIRST;
+
+    IO::AIO::aio_readdirx $self->{to_string}, $flags, sub ( $res, $flags ) {
+        $res = $cb->($res) if defined $cb;
+
+        $cv->($res) if defined $cv;
+
+        return;
+    };
+
+    return defined $cv ? $cv->recv : ();
+}
 
 1;
 __END__
