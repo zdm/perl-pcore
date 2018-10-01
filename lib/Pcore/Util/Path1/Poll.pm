38f93c4b5361cdbfde8f9a1878c6183bcc698178
zdm <zdm@softvisio.net>
2018-10-02 02:29:30 +0300

    path1 prototype

--- lib/Pcore/Util/Path1/Poll.pm
+++ lib/Pcore/Util/Path1/Poll.pm
@@ -1,70 +1,116 @@
 package Pcore::Util::Path1::Poll;
 
 use Pcore -role, -const;
+use Pcore::Util::Scalar qw[is_plain_coderef];
+use Time::HiRes qw[];
+use Coro::Signal qw[];
 
-our $POLL;
-our $POLL_THREAD;
-our $POLL_TIMEOUT = 3;
+const our $DEFAULT_POLL_INTERVAL => 3;
 
 sub poll ( $self, @ ) {
-    my $cb = is_plain_coderef $_[-1] ? shift : ();
+    state $POLL_INTERVAL = $DEFAULT_POLL_INTERVAL;
+    state $POLL;
+    state $SIGNAL = Coro::Signal->new;
+    state $thread;
 
-    if ($cb) {
-        $POLL->{$self}->{ refaddr $cb } = $cb;
+    my $cb = is_plain_coderef $_[-1] ? pop : ();
+
+    my %args = @_[ 1 .. $#_ ];
+
+    my $interval = $args{interval} || $DEFAULT_POLL_INTERVAL;
+
+    my $path = $self->to_abs;
+
+    my $poll = $POLL->{$path} = {
+        path     => $path,
+        interval => $interval,
+        last     => 0,
+        cb       => $cb,
+    };
+
+    $POLL_INTERVAL = $interval if $interval < $POLL_INTERVAL;
+
+    if ( -e $path ) {
+        $poll->{stat}->{$path} = [ Time::HiRes::stat($path) ];
+
+        if ( -d _ && ( my $files = $path->read_dir( abs => 1, recursive => 1, dir => 0 ) ) ) {
+            for my $file ( $files->@* ) {
+                $poll->{stat}->{$file} = [ Time::HiRes::stat($file) ];
+            }
+        }
     }
 
-    _poll_path($self);
-
-    _poll_run_thread() if !$POLL_THREAD;
-
-    return;
-}
-
-sub _poll_path ($path) {
-    $path->stat;
-
-    if ( -d $path ) {
-
-    }
-
-    IO::AIO::aio_stat(
-        "$path",
-        sub ($error) {
-
-            # die if $error;
-
-            # my $mtime = IO::AIO::st_mtime;
-
-            # if ( !exists $files->{$path} || $files->{$path} != $mtime ) {
-            #     $files->{$path} = $mtime;
-
-            #     $changed = 1;
-            # }
-
-            # $cv->end;
-
-            return;
-        }
-    );
-
-    return;
-}
-
-sub _poll_run_thread {
-    $POLL_THREAD = 1;
-
-    Coro::async {
-        while () {
-            Coro::AnyEvent::sleep $POLL_TIMEOUT;
-        }
+    if ($thread) {
+        $SIGNAL->send if $SIGNAL->awaited;
 
         return;
+    }
+
+    $thread = Coro::async {
+        while () {
+            Coro::AnyEvent::sleep $POLL_INTERVAL;
+
+            for my $poll ( values $POLL->%* ) {
+                next if $poll->{last} + $poll->{interval} > time;
+
+                $poll->{last} = time;
+
+                say $poll->{path};
+
+                my $stat;
+
+                if ( -e $poll->{path} ) {
+                    $stat->{ $poll->{path} } = [ Time::HiRes::stat(_) ];
+
+                    if ( -d _ && ( my $files = $poll->{path}->read_dir( abs => 1, recursive => 1, dir => 0 ) ) ) {
+                        for my $file ( $files->@* ) {
+                            $stat->{$file} = [ Time::HiRes::stat($file) ];
+                        }
+                    }
+                }
+
+                my @changes;
+
+                for my $file ( keys $stat->%* ) {
+                    if ( exists $poll->{stat}->{$file} ) {
+                        push @changes, [ $file, 'modified' ] if $poll->{stat}->{$file}->[9] != $stat->{$file}->[9];
+                    }
+                    else {
+                        push @changes, [ $file, 'created' ];
+                    }
+
+                    $poll->{stat}->{$file} = $stat->{$file};
+                }
+
+                for my $file ( keys $poll->{stat}->%* ) {
+                    if ( !exists $stat->{$file} ) {
+                        delete $poll->{stat}->{$file};
+
+                        push @changes, [ $file, 'removed' ];
+                    }
+                }
+
+                $poll->{cb}->( \@changes ) if @changes;
+            }
+
+            $SIGNAL->wait if !$POLL->%*;
+        }
     };
 
     return;
 }
 
 1;
+## -----SOURCE FILTER LOG BEGIN-----
+##
+## PerlCritic profile "pcore-script" policy violations:
+## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
+## | Sev. | Lines                | Policy                                                                                                         |
+## |======+======================+================================================================================================================|
+## |    3 | 10                   | Subroutines::ProhibitExcessComplexity - Subroutine "poll" with high complexity score (25)                      |
+## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
+##
+## -----SOURCE FILTER LOG END-----
 __END__
 =pod
 
