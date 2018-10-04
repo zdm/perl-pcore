97a62c913621e0b2b84a9631a516de493057e677
zdm <zdm@softvisio.net>
2018-10-04 03:38:05 +0300

    path1 prototype

--- lib/Pcore/Util/Path1/Dir.pm
+++ lib/Pcore/Util/Path1/Dir.pm
@@ -4,19 +4,25 @@ use Pcore -role;
 use Pcore::Util::Scalar qw[is_plain_coderef];
 use Fcntl qw[];
 
-# abs, recursive, dir, file
-sub read_dir ( $self, %args ) {
+sub read_dir ( $self, @ ) {
     my $res;
 
-    $args{dir}  //= 1;
-    $args{file} //= 1;
+    my %args = (
+        max_depth   => 1,
+        follow_link => 1,
+        is_dir      => 1,
+        is_file     => 1,
+        is_sock     => 1,
+        is_link     => undef,    # undef - do not check, 1 - add links, 0 - skip links
+        @_[ 1 .. $#_ ]
+    );
 
     # must be without trailing '/'
     my $base = $self->to_abs->{to_string};
 
     my $prefix = $args{abs} ? $self->to_abs->{to_string} . '/' : '';
 
-    my $read = sub ($dir) {
+    my $read = sub ( $dir, $depth ) {
         my $dir_path = "${base}$dir";
 
         opendir my $dh, $dir_path or die qq[Can't open dir "$dir_path"];
@@ -30,22 +36,64 @@ sub read_dir ( $self, %args ) {
 
             my $fpath = "${dir_path}$path";
 
-            my $rel_dir = substr $dir, 1;
+            my ( $stat, $lstat );
 
-            if ( -f $fpath ) {
-                push $res->@*, "${prefix}${rel_dir}$path" if $args{file};
+            my $push = 1;
+
+            if ( defined $args{is_link} ) {
+                $lstat //= ( lstat $fpath )[2] & Fcntl::S_IFMT;
+
+                if ( $lstat == Fcntl::S_IFLNK ) {
+                    $push = 0 if !$args{is_link};
+                }
+                else {
+                    $push = 0 if $args{is_link};
+                }
             }
-            elsif ( -d _ ) {
-                push $res->@*, "${prefix}${rel_dir}$path" if $args{dir};
 
-                __SUB__->("${dir}$path/") if $args{recursive};
+            if ( $push && !$args{is_file} ) {
+                $stat //= ( stat $fpath )[2] & Fcntl::S_IFMT;
+
+                $push = 0 if $stat == Fcntl::S_IFREG;
+            }
+
+            if ( $push && !$args{is_dir} ) {
+                $stat //= ( stat $fpath )[2] & Fcntl::S_IFMT;
+
+                $push = 0 if $stat == Fcntl::S_IFDIR;
+            }
+
+            if ( $push && !$args{is_sock} ) {
+                $stat //= ( stat $fpath )[2] & Fcntl::S_IFMT;
+
+                $push = 0 if $stat == Fcntl::S_IFSOCK;
+            }
+
+            if ($push) {
+                my $rel_dir = substr $dir, 1;
+
+                push $res->@*, "${prefix}${rel_dir}$path";
+            }
+
+            if ( !$args{max_depth} || $depth < $args{max_depth} ) {
+                $stat //= ( stat $fpath )[2] & Fcntl::S_IFMT;
+
+                if ( $stat == Fcntl::S_IFDIR ) {
+                    if ( !$args{follow_link} ) {
+                        $lstat //= ( lstat $fpath )[2] & Fcntl::S_IFMT;
+
+                        next if $lstat == Fcntl::S_IFLNK;
+                    }
+
+                    __SUB__->( "${dir}$path/", $depth + 1 );
+                }
             }
         }
 
         return;
     };
 
-    $read->('/');
+    $read->( '/', 1 );
 
     return $res;
 }
@@ -57,7 +105,11 @@ sub read_dir ( $self, %args ) {
 ## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
 ## | Sev. | Lines                | Policy                                                                                                         |
 ## |======+======================+================================================================================================================|
-## |    2 | 17                   | ValuesAndExpressions::ProhibitEmptyQuotes - Quotes used with a string containing no non-whitespace characters  |
+## |    3 | 7                    | Subroutines::ProhibitExcessComplexity - Subroutine "read_dir" with high complexity score (28)                  |
+## |------+----------------------+----------------------------------------------------------------------------------------------------------------|
+## |    2 | 23                   | ValuesAndExpressions::ProhibitEmptyQuotes - Quotes used with a string containing no non-whitespace characters  |
+## |------+----------------------+----------------------------------------------------------------------------------------------------------------|
+## |    1 | 10                   | CodeLayout::RequireTrailingCommas - List declaration without trailing comma                                    |
 ## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
 ##
 ## -----SOURCE FILTER LOG END-----
