ff1edf4208d88beb8514f9c4e4a14b9c5f1c61de
zdm <zdm@softvisio.net>
2018-10-31 21:29:24 +0200

    ENV refactored

--- lib/Pcore/Core/Env/Share.pm
+++ lib/Pcore/Core/Env/Share.pm
@@ -84,17 +84,15 @@ sub get ( $self, @ ) {
     return;
 }
 
-sub read_cfg ( $self, @args ) {
-    return P->cfg->read( $self->get(@args) );
-}
+sub read_cfg ( $self, @args ) { return P->cfg->read( $self->get(@args) ) }
 
 sub write ( $self, $lib, $path, $file ) {    ## no critic qw[Subroutines::ProhibitBuiltinHomonyms]
     die qq[share lib "$lib" is not exists] if !exists $self->{_lib_idx}->{$lib};
 
-    $path = P->path( $self->{_lib_idx}->{$lib} . $path );
+    $path = P->path1("$self->{_lib_idx}->{$lib}/$path");
 
     # create path
-    P->file->mkpath( $path->dirname ) if !-d $path->dirname;
+    P->file->mkpath( $path->{dirname} ) if !-d $path->{dirname};
 
     # create file
     if ( is_plain_scalarref $file ) {
