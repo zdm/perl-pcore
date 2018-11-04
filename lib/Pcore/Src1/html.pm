1d06476ce5ceb49313fe4881fb6dba3c85bc0711
zdm <zdm@softvisio.net>
2018-11-05 00:26:46 +0200

    src redesign #no filter

--- lib/Pcore/Src1/html.pm
+++ lib/Pcore/Src1/html.pm
@@ -2,74 +2,61 @@ package Pcore::Src1::html;
 
 use Pcore -class, -res;
 use Pcore::Util::Text qw[trim];
-use Pcore::Src::Filter::JS;
-use Pcore::Src::Filter::CSS;
+use Pcore::Src1::js;
+use Pcore::Src1::css;
 
 with qw[Pcore::Src1::Filter];
 
-# HTML_BEAUTIFY => q[--indent-scripts normal],
-# HTML_PACKER_MINIFY => {
-#     remove_comments => 0,
-#     remove_newlines => 1,
-#     html5           => 1,
-# },
-
 sub decompress ($self) {
-    return res 200 if !length $self->{buffer}->$*;
+    return res 200 if !length $self->{data}->$*;
 
     return res 200 if $self->has_kolon;
 
-    my $html_beautify_args = $self->dist_cfg->{HTML_BEAUTIFY} || $self->src_cfg->{HTML_BEAUTIFY};
-
     my $temp = P->file1->tempfile;
 
-    P->file->write_bin( $temp, $self->{buffer} );
+    P->file->write_bin( $temp, $self->{data} );
 
-    my $proc = P->sys->run_proc( qq[html-beautify $html_beautify_args --replace "$temp"], win32_create_no_window => 1 )->wait;
+    my $proc = P->sys->run_proc( qq[html-beautify --indent-scripts normal --replace "$temp"], win32_create_no_window => 1 )->wait;
 
-    $self->{buffer}->$* = P->file->read_bin($temp)->$*;    ## no critic qw[Variables::RequireLocalizedPunctuationVars]
+    $self->{data}->$* = P->file->read_bin($temp)->$*;    ## no critic qw[Variables::RequireLocalizedPunctuationVars]
 
     return res 200;
 }
 
 sub compress ($self) {
-    return 0 if !length $self->{buffer}->$*;
+    return res 200 if !length $self->{data}->$*;
 
-    return 0 if $self->has_kolon;
+    return res 200 if $self->has_kolon;
 
     # compress js
-    my @script = split m[(<script[^>]*>)(.*?)(</script[^>]*>)]smi, $self->{buffer}->$*;
+    my @script = split m[(<script[^>]*>)(.*?)(</script[^>]*>)]smi, $self->{data}->$*;
 
     for my $i ( 0 .. $#script ) {
         if ( $script[$i] =~ m[\A</script]sm && $script[ $i - 1 ] ) {
-            Pcore::Src::Filter::JS->new( { file => $self->file, buffer => \$script[ $i - 1 ] } )->compress;
+            Pcore::Src1::js->new( { file => $self->{file}, data => \$script[ $i - 1 ] } )->compress;
 
             trim $script[ $i - 1 ];
         }
     }
 
-    $self->{buffer}->$* = join q[], @script;    ## no critic qw[Variables::RequireLocalizedPunctuationVars]
+    $self->{data}->$* = join '', @script;    ## no critic qw[Variables::RequireLocalizedPunctuationVars]
 
     # compress css
-    my @css = split m[(<style[^>]*>)(.*?)(</style[^>]*>)]smi, $self->{buffer}->$*;
+    my @css = split m[(<style[^>]*>)(.*?)(</style[^>]*>)]smi, $self->{data}->$*;
 
     for my $i ( 0 .. $#css ) {
         if ( $css[$i] =~ m[\A</style]sm && $css[ $i - 1 ] ) {
-            Pcore::Src::Filter::CSS->new( { file => $self->file, buffer => \$css[ $i - 1 ] } )->compress;
+            Pcore::Src1::css->new( { file => $self->{file}, data => \$css[ $i - 1 ] } )->compress;
         }
     }
 
-    $self->{buffer}->$* = join q[], @css;       ## no critic qw[Variables::RequireLocalizedPunctuationVars]
-
-    my $html_packer_minify_args = $self->dist_cfg->{HTML_PACKER_MINIFY} || $self->src_cfg->{HTML_PACKER_MINIFY};
+    $self->{data}->$* = join '', @css;       ## no critic qw[Variables::RequireLocalizedPunctuationVars]
 
     require HTML::Packer;
 
-    eval {
-        $self->{buffer}->$* = HTML::Packer->init->minify( $self->{buffer}, $html_packer_minify_args );    ## no critic qw[Variables::RequireLocalizedPunctuationVars]
-    };
+    eval { $self->{data}->$* = HTML::Packer->init->minify( $self->{data}, { remove_comments => 0, remove_newlines => 1, html5 => 1 } ) };    ## no critic qw[Variables::RequireLocalizedPunctuationVars]
 
-    return 0;
+    return res 200;
 }
 
 1;
@@ -79,7 +66,9 @@ sub compress ($self) {
 ## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
 ## | Sev. | Lines                | Policy                                                                                                         |
 ## |======+======================+================================================================================================================|
-## |    3 | 68                   | ErrorHandling::RequireCheckingReturnValueOfEval - Return value of eval not tested                              |
+## |    3 | 57                   | ErrorHandling::RequireCheckingReturnValueOfEval - Return value of eval not tested                              |
+## |------+----------------------+----------------------------------------------------------------------------------------------------------------|
+## |    2 | 42, 53               | ValuesAndExpressions::ProhibitEmptyQuotes - Quotes used with a string containing no non-whitespace characters  |
 ## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
 ##
 ## -----SOURCE FILTER LOG END-----
