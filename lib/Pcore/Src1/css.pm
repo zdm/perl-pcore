72170fa606eea992bba01c83f5e890e20989ad6f
zdm <zdm@softvisio.net>
2018-11-04 22:55:53 +0200

    src redesign

--- lib/Pcore/Src1/css.pm
+++ lib/Pcore/Src1/css.pm
@@ -13,7 +13,7 @@ my $DECOMPRESSOR = do {
     $packer->{old_declaration_replacement} = $packer->{declaration}->{reggrp_data}->[0]->{replacement};
 
     $packer->{declaration}->{reggrp_data}->[0]->{replacement} = sub {
-        return ' ' x 4 . $packer->{old_declaration_replacement}->(@_);
+        return q[ ] x 4 . $packer->{old_declaration_replacement}->(@_);
     };
 
     $packer->{_reggrp_declaration} = Regexp::RegGrp->new( { reggrp => $packer->{declaration}->{reggrp_data} } );
@@ -34,16 +34,6 @@ sub compress ($self) {
 }
 
 1;
-## -----SOURCE FILTER LOG BEGIN-----
-##
-## PerlCritic profile "pcore-script" policy violations:
-## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
-## | Sev. | Lines                | Policy                                                                                                         |
-## |======+======================+================================================================================================================|
-## |    2 | 16                   | ValuesAndExpressions::ProhibitEmptyQuotes - Quotes used with a string containing no non-whitespace characters  |
-## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
-##
-## -----SOURCE FILTER LOG END-----
 __END__
 =pod
 
