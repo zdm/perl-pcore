28b413921b8624a6eabc424371a4ab4902e217a4
zdm <zdm@softvisio.net>
2019-06-02 13:11:50 +0300

    perl 5.30

--- lib/Pcore.pm
+++ lib/Pcore.pm
@@ -1,6 +1,6 @@
 package Pcore v0.87.0;
 
-use v5.28;
+use v5.30;
 no strict qw[refs];    ## no critic qw[TestingAndDebugging::ProhibitProlongedStrictureOverride]
 use common::header;
 use Pcore::Core::Exporter qw[];
