a07413aea7db15b42cd2582ef443efd4f282c52c
zdm <zdm@softvisio.net>
2019-11-23 08:56:46 +0200

    node refactored

--- lib/Pcore/Node/Node.pm
+++ lib/Pcore/Node/Node.pm
@@ -42,26 +42,22 @@ sub run ( $type, $args ) {
                 };
             }
         },
-        on_rpc => sub ( $self, $req, $tx ) {
+        on_rpc => sub ( $self, $tx ) {
             my $method_name = "API_$tx->{method}";
 
             if ( my $sub = $node->can($method_name) ) {
 
                 # call method
-                eval { $node->$sub( $req, $tx->{args} ? $tx->{args}->@* : () ) };
-
-                $@->sendlog if $@;
+                return $node->$sub( $tx->{args} ? $tx->{args}->@* : () );
             }
             else {
-                $req->( [ 400, q[Method not implemented] ] );
+                return [ 400, q[Method not implemented] ];
             }
-
-            return;
         },
     );
 
     # handshake
-    Coro::async_pool sub {
+    Coro::async sub {
 
         # open control handle
         my $fh = gensym;
@@ -97,16 +93,6 @@ sub run ( $type, $args ) {
 }
 
 1;
-## -----SOURCE FILTER LOG BEGIN-----
-##
-## PerlCritic profile "pcore-script" policy violations:
-## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
-## | Sev. | Lines                | Policy                                                                                                         |
-## |======+======================+================================================================================================================|
-## |    3 | 51                   | ErrorHandling::RequireCheckingReturnValueOfEval - Return value of eval not tested                              |
-## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
-##
-## -----SOURCE FILTER LOG END-----
 __END__
 =pod
 
