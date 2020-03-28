5a5032a7084ffc43c3f31b82a254fff6c9d0590d
zdm <zdm@softvisio.net>
2020-03-28 07:00:56 +0200

    moz api added

--- lib/Pcore/API/Moz.pm
+++ lib/Pcore/API/Moz.pm
@@ -15,6 +15,8 @@ has _auth        => ( is => 'lazy' );
 has _semaphore   => sub ($self) { Coro::Semaphore->new( $self->{max_threads} ) }, is => 'lazy';
 has _last_req_ts => ();
 
+# https://moz.com/help/links-api, click on "making calls" to open api endpoints
+
 sub BUILD ( $self, $args ) {
     if ( $self->{free} ) {
         $self->{max_threads}  = 1;
@@ -43,6 +45,60 @@ sub get_anchor_text ( $self, $target, $scope, %args ) {
     return $res;
 }
 
+# https://moz.com/help/links-api/making-calls/final-redirect
+# TODO
+sub get_final_redirect ($self) {
+    ...;
+}
+
+# https://moz.com/help/links-api/making-calls/global-top-pages
+# TODO
+sub get_global_top_pages ($self) {
+    ...;
+}
+
+# https://moz.com/help/links-api/making-calls/global-top-root-domains
+# TODO
+sub get_global_top_root_domains ($self) {
+    ...;
+}
+
+# https://moz.com/help/links-api/making-calls/index-metadata
+# TODO
+sub get_index_metadata ($self) {
+    ...;
+}
+
+# https://moz.com/help/links-api/making-calls/link-intersect
+# TODO
+sub get_link_intersect ($self) {
+    ...;
+}
+
+# https://moz.com/help/links-api/making-calls/link-status
+# TODO
+sub get_link_status ($self) {
+    ...;
+}
+
+# https://moz.com/help/links-api/making-calls/linking-root-domains
+# TODO
+sub get_linking_root_domains ($self) {
+    ...;
+}
+
+# https://moz.com/help/links-api/making-calls/link-metrics
+# TODO
+sub get_links ($self) {
+    ...;
+}
+
+# https://moz.com/help/links-api/making-calls/top-pages-metrics
+# TODO
+sub get_top_pages ($self) {
+    ...;
+}
+
 # https://moz.com/help/links-api/making-calls/url-metrics
 # urls - up to 50 urls
 # da = domain_authority - Domain Authority, a normalized 100-point score representing the likelihood of a domain to rank well in search engine results
@@ -52,6 +108,12 @@ sub get_url_metrics ( $self, $url, %args ) {
     my $res = $self->_req( 'url_metrics', { targets => is_plain_arrayref $url ? $url : [$url], } );
 
     return $res;
+}
+
+# https://moz.com/help/links-api/making-calls/usage-data
+# TODO
+sub get_usage_data ($self) {
+    ...;
 }
 
 sub _req ( $self, $endpoint, $params ) {
@@ -86,6 +148,17 @@ sub _req ( $self, $endpoint, $params ) {
 }
 
 1;
+## -----SOURCE FILTER LOG BEGIN-----
+##
+## PerlCritic profile "pcore-script" policy violations:
+## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
+## | Sev. | Lines                | Policy                                                                                                         |
+## |======+======================+================================================================================================================|
+## |    3 | 51, 57, 63, 69, 75,  | ControlStructures::ProhibitYadaOperator - yada operator (...) used                                             |
+## |      | 81, 87, 93, 99, 116  |                                                                                                                |
+## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
+##
+## -----SOURCE FILTER LOG END-----
 __END__
 =pod
 
