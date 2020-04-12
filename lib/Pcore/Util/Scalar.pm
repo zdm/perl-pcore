80e43a91c87a113acaf8f29a20584674d7ee3ea4
zdm <zdm@softvisio.net>
2020-04-12 12:39:40 +0300

    is_bool method added

--- lib/Pcore/Util/Scalar.pm
+++ lib/Pcore/Util/Scalar.pm
@@ -37,7 +37,8 @@ sub on_destroy ( $scalar, $cb ) {
 }
 
 sub is_bool : prototype($) {
-    return ( is_ref $_[0] and UNIVERSAL::isa( $_[0], JSON::PP::Boolean:: ) ) or ( exists $INC{'Types/Serialiser.pm'} and Types::Serialiser::is_bool( $_[0] ) );
+
+    return ( is_ref $_[0] && UNIVERSAL::isa( $_[0], JSON::PP::Boolean:: ) ) || ( exists $INC{'Types/Serialiser.pm'} && Types::Serialiser::is_bool( $_[0] ) );
 }
 
 sub is_path : prototype($) { return is_blessed_hashref $_[0] && $_[0]->isa('Pcore::Util::Path') }
@@ -55,7 +56,7 @@ sub is_res : prototype($) { return is_blessed_hashref $_[0] && $_[0]->can('IS_PC
 ## |======+======================+================================================================================================================|
 ## |    3 | 16                   | RegularExpressions::ProhibitComplexRegexes - Split long regexps into smaller qr// chunks                       |
 ## |------+----------------------+----------------------------------------------------------------------------------------------------------------|
-## |    3 | 40                   | BuiltinFunctions::ProhibitUniversalIsa - UNIVERSAL::isa should not be used as a function                       |
+## |    3 | 41                   | BuiltinFunctions::ProhibitUniversalIsa - UNIVERSAL::isa should not be used as a function                       |
 ## +------+----------------------+----------------------------------------------------------------------------------------------------------------+
 ##
 ## -----SOURCE FILTER LOG END-----
